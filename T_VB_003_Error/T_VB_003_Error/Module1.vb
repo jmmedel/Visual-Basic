7589e7966736ecf57639052d6fdd1c7253b993b4
john <jm.kagaya@gmail.com>
2018-03-10 13:49:36 +0900

    Done
    Done

--- T_VB_003_Error/T_VB_003_Error/Module1.vb
+++ T_VB_003_Error/T_VB_003_Error/Module1.vb
@@ -3,6 +3,9 @@
     Sub Main()
         Console.WriteLine("Hello world")
         Console.ReadLine()
+        'Expression expected ' 
+        'logic error  '
+        ' Major error'
     End Sub
 
 End Module
---
+++ T_VB_004_Variables/T_VB_004_Variables/Module1.vb
@@ -0,0 +1,19 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim mynum As Integer
+        mynum = 5
+        Dim mystring As String = "Hello world"
+        Dim mychar As Char = "J"
+        Dim mydouble As Double = 3.5
+        Dim mybool As Boolean = True
+        Console.WriteLine(mybool)
+        Console.WriteLine(mydouble)
+        Console.WriteLine(mynum)
+        Console.WriteLine(mystring)
+        Console.WriteLine(mychar)
+        Console.ReadLine()
+        'In VB you dont need ; '
+    End Sub
+
+End Module
