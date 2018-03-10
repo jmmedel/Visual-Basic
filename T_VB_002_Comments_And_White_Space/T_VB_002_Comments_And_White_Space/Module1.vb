b49ad04f29427ccd0b6bc8f1ac14ff91e039646d
john <jm.kagaya@gmail.com>
2018-03-10 13:38:56 +0900

    Done
    Done

---
+++ T_VB_002_Comments_And_White_Space/T_VB_002_Comments_And_White_Space/Module1.vb
@@ -0,0 +1,13 @@
+﻿Module Module1
+
+    Sub Main()
+        'This is line write the text heelo world to the console'
+        Console.WriteLine("Hello world")
+        'This is line pause the program and lets the user add read the text'
+        'Test'
+        Console.ReadLine()
+
+
+    End Sub
+
+End Module
---
+++ T_VB_002_Comments_And_White_Space/T_VB_002_Comments_And_White_Space/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("T_VB_002_Comments_And_White_Space")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("T_VB_002_Comments_And_White_Space")>
+<Assembly: AssemblyCopyright("Copyright ©  2018")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("cb3b2a21-d3ea-4b17-9b49-e2e3b8421e23")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
