3dc9ef4d2402cb7b2a0755be8dbda16d6d32f232
john <jm.kagaya@gmail.com>
2018-03-10 13:57:49 +0900

    Done
    Done

--- T_VB_005_Math_Operator/T_VB_005_Math_Operator/Module1.vb
+++ T_VB_005_Math_Operator/T_VB_005_Math_Operator/Module1.vb
@@ -1,7 +1,24 @@
 ﻿Module Module1
 
     Sub Main()
+        Dim num1 As Integer
+        Dim num2 As Integer
+        Dim num3 As Integer
+        Dim num4 As Integer
+        Dim answer As Integer = Nothing
+        num1 = 5 + 5
+        num2 = 5 - 5
+        num3 = 5 * 5
+        num4 = 5 / 5
+        answer = num1 + num2
 
+        Console.WriteLine(num1)
+        Console.WriteLine(num2)
+        Console.WriteLine(num3)
+        Console.WriteLine(num4)
+        Console.WriteLine("This is the answer")
+        Console.WriteLine(answer)
+        Console.ReadLine()
     End Sub
 
 End Module
