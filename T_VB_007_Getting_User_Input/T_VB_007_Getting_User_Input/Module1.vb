207b5f8d4ab43678f1a920bcf1956b7216820bd9
john <jm.kagaya@gmail.com>
2018-03-10 14:22:59 +0900

    Done
    Done

--- T_VB_007_Getting_User_Input/T_VB_007_Getting_User_Input/Module1.vb
+++ T_VB_007_Getting_User_Input/T_VB_007_Getting_User_Input/Module1.vb
@@ -1,7 +1,20 @@
 ﻿Module Module1
 
     Sub Main()
+        Dim username As String = Nothing
+        Dim userAge As Integer = Nothing
+        Dim usersalary As Double = Nothing
+        Console.WriteLine("What is your name?")
+        username = Console.ReadLine()
+        Console.WriteLine("What is your age")
+        userAge = Console.ReadLine()
+        Console.WriteLine("What is your salary?")
+        usersalary = Console.ReadLine()
+        Console.Write("   Youname is " & username)
+        Console.Write("   You age is " & userAge)
+        Console.Write("  You salary is " & usersalary)
 
+        Console.ReadLine()
     End Sub
 
 End Module
