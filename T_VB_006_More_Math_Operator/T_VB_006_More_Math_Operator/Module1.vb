14c4d11be3359cf7f11e91715ebdc63da3e8701f
john <jm.kagaya@gmail.com>
2018-03-10 14:09:45 +0900

    Done
    Done

---
+++ T_VB_006_More_Math_Operator/T_VB_006_More_Math_Operator/Module1.vb
@@ -0,0 +1,12 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim myDouble As Double = 11
+        Console.WriteLine("This is initial Value : " & myDouble)
+        myDouble += 23
+        'This is after adding it to the variable'
+        Console.WriteLine("This is the answer " & myDouble)
+        Console.ReadLine()
+    End Sub
+
+End Module
