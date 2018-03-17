298621071fd36f437bca8e9e86dad0e02395ab4c
john <jm.kagaya@gmail.com>
2018-03-17 11:38:31 +0900

    Done
    Done

--- T_VB_008_Calculator/T_VB_008_Calculator/Module1.vb
+++ T_VB_008_Calculator/T_VB_008_Calculator/Module1.vb
@@ -1,7 +1,14 @@
 ﻿Module Module1
     '12'
     Sub Main()
-
+        Console.WriteLine("Please enter the first number:")
+        Dim num1 As Double = Console.ReadLine()
+        Console.WriteLine("First number: " & num1)
+        Console.WriteLine("Please enter the second number")
+        Dim num2 As Double = Console.ReadLine()
+        Console.WriteLine("Second number:" & num2)
+        Console.WriteLine("Answer" & num1 * num2)
+        Console.ReadLine()
     End Sub
 
 End Module
