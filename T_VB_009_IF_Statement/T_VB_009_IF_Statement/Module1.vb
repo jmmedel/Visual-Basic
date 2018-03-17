03b4b7e003f36585af9c0e4ee610a671e1f51ba1
john <jm.kagaya@gmail.com>
2018-03-17 11:44:45 +0900

    Done
    Done

--- T_VB_009_IF_Statement/T_VB_009_IF_Statement/Module1.vb
+++ T_VB_009_IF_Statement/T_VB_009_IF_Statement/Module1.vb
@@ -2,6 +2,17 @@
 
     Sub Main()
 
+        Console.WriteLine("What is your name_")
+        Dim username As String = Console.ReadLine()
+        Console.WriteLine("What is your password")
+        Dim password As String = Console.ReadLine()
+        If username = "Sam" Then
+            Console.WriteLine("Welcome sam!")
+        End If
+        If password = "123456789" Then
+            Console.WriteLine("You enter rhe corred password")
+        End If
+        Console.ReadLine()
     End Sub
 
 End Module
