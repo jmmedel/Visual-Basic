82f91bda04e909a5a398f61ed4453b2f0c652566
john <jm.kagaya@gmail.com>
2018-03-17 12:16:51 +0900

    Done
    Done

--- T_VB_011_Nested_IF/T_VB_011_Nested_IF/Module1.vb
+++ T_VB_011_Nested_IF/T_VB_011_Nested_IF/Module1.vb
@@ -1,6 +1,23 @@
 ﻿Module Module1
 
     Sub Main()
+        Dim Age As Integer = Nothing
+        Dim hasInsurance As Boolean = Nothing
+        Console.WriteLine("What is your age?")
+        Age = Console.ReadLine()
+        Console.WriteLine("Do you have insurance true or false")
+        hasInsurance = Console.ReadLine()
+        If Age = 16 Then
+            If hasInsurance = True Then
+                Console.WriteLine("You can drive legally")
+            Else
+                Console.WriteLine("You can drive but dont get crash")
+            End If
+        Else
+            Console.WriteLine("You cannot drive")
+        End If
+
+
 
     End Sub
 
