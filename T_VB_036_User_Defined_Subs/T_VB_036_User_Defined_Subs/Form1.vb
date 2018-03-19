7f71467d8c60972eaeea94d477679d09b29e07b4
john <jm.kagaya@gmail.com>
2018-03-19 23:45:52 +0900

    Done
    Done

--- T_VB_036_User_Defined_Subs/T_VB_036_User_Defined_Subs/Form1.vb
+++ T_VB_036_User_Defined_Subs/T_VB_036_User_Defined_Subs/Form1.vb
@@ -1,3 +1,14 @@
 ﻿Public Class Form1
 
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        addnumber()
+    End Sub
+
+    Private Sub addnumber()
+        Dim num1 As Integer = 23123
+        Dim num2 As Integer = 51241
+        MessageBox.Show(num1 + num2)
+
+    End Sub
+
 End Class
