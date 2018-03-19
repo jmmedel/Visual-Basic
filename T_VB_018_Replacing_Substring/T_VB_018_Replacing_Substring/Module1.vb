47bab84af605600cd8c57a5a2e4c325a16cb3003
john <jm.kagaya@gmail.com>
2018-03-19 21:58:59 +0900

    Done
    Done

--- T_VB_018_Replacing_Substring/T_VB_018_Replacing_Substring/Module1.vb
+++ T_VB_018_Replacing_Substring/T_VB_018_Replacing_Substring/Module1.vb
@@ -2,6 +2,14 @@
 
     Sub Main()
 
+        Dim mystring As String = Nothing
+        Dim finalstring As String = Nothing
+        Console.WriteLine("Please enter a string ")
+        mystring = Console.ReadLine()
+        finalstring = mystring.Replace("a", "n")
+        Console.WriteLine(finalstring)
+        Console.ReadLine()
+
     End Sub
 
 End Module
