717b8d4fa2a7478c2a334f7097cccf96ca8a94e0
john <jm.kagaya@gmail.com>
2018-03-19 23:42:49 +0900

    Done
    Done

--- T_VB_035_Input_Box/T_VB_035_Input_Box/Form1.vb
+++ T_VB_035_Input_Box/T_VB_035_Input_Box/Form1.vb
@@ -1,3 +1,8 @@
 ﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim username As String = Nothing
+        username = InputBox("What is your name? ", "heelo",,,)
+        Label1.Text = "Heelo " & username
+    End Sub
 
 End Class
---
+++ T_VB_036_User_Defined_Subs/T_VB_036_User_Defined_Subs/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("T_VB_036_User_Defined_Subs")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("T_VB_036_User_Defined_Subs")>
+<Assembly: AssemblyCopyright("Copyright ©  2018")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("493c5151-ec45-487c-a45a-a33d20fe2eb6")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
