bfb16ee6b7475a7b258957d7300ff95bd51a8425
john <jm.kagaya@gmail.com>
2018-03-19 22:23:15 +0900

    Done
    done

--- T_VB_022_For_Next_Loop/T_VB_022_For_Next_Loop/Module1.vb
+++ T_VB_022_For_Next_Loop/T_VB_022_For_Next_Loop/Module1.vb
@@ -1,7 +1,12 @@
 ﻿Module Module1
 
     Sub Main()
+        Dim int1 As Integer
+        For index = 1 To 10
+            Console.WriteLine("Int1 = " & index)
 
+        Next
+        Console.ReadLine()
     End Sub
 
 End Module
