278c31dfffca7ec55903398e98db7775a7582e7c
john <jm.kagaya@gmail.com>
2018-03-19 21:35:46 +0900

    Done
    Done

--- T_VB_014_Concatenate_Strings/T_VB_014_Concatenate_Strings/Module1.vb
+++ T_VB_014_Concatenate_Strings/T_VB_014_Concatenate_Strings/Module1.vb
@@ -2,6 +2,15 @@
 
     Sub Main()
 
+        Dim userstring As String = Nothing
+        Dim programstring As String = "catfish"
+        Console.WriteLine("Type anything you want")
+        userstring = Console.ReadLine()
+        Console.WriteLine(userstring)
+        userstring = userstring + " " + programstring
+        Console.WriteLine(userstring & " Hello youtube ")
+        Console.ReadLine()
+
     End Sub
 
 End Module
