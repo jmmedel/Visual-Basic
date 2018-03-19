6f69be55289321f2c3c4cf48e97dbe1db658c076
john <jm.kagaya@gmail.com>
2018-03-19 23:11:56 +0900

    Done
    Done

--- T_VB_030_Nested_Loops/T_VB_030_Nested_Loops/Module1.vb
+++ T_VB_030_Nested_Loops/T_VB_030_Nested_Loops/Module1.vb
@@ -1,7 +1,15 @@
 ﻿Module Module1
 
     Sub Main()
+        For index = 1 To 10
+            For num2 = 1 To 10
+                Console.Write(num2)
+            Next
+            Console.WriteLine()
 
+        Next
+
+        Console.ReadLine()
     End Sub
 
 End Module
