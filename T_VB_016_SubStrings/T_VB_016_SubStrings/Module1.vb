acc2ccf63a95a3207722e1291dbeb4011ee15b57
john <jm.kagaya@gmail.com>
2018-03-19 21:48:57 +0900

    Done
    Done

--- T_VB_016_SubStrings/T_VB_016_SubStrings/Module1.vb
+++ T_VB_016_SubStrings/T_VB_016_SubStrings/Module1.vb
@@ -2,6 +2,16 @@
 
     Sub Main()
 
+        Dim userstring As String = Nothing
+        Console.WriteLine("Enter a string")
+        userstring = Console.ReadLine()
+        Console.WriteLine()
+        Console.WriteLine(userstring.Length.ToString())
+        Console.WriteLine(userstring.Substring(0, 3))
+        Console.WriteLine(userstring.Substring(3, 3))
+        Console.WriteLine(userstring.Substring(6))
+
+        Console.ReadLine()
     End Sub
 
 End Module
