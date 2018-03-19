254a5f83b89756943033e548dc9a4f2781c1a72c
john <jm.kagaya@gmail.com>
2018-03-20 00:09:39 +0900

    Done
    Done

--- T_VB_039_ByRef/T_VB_039_ByRef/Form1.vb
+++ T_VB_039_ByRef/T_VB_039_ByRef/Form1.vb
@@ -1,3 +1,17 @@
 ﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim num1 As Integer = 0
+        MessageBox.Show(num1.ToString())
+        Label1.Text = num1.ToString()
+        increamentVariable(num1)
+        MessageBox.Show(num1.ToString())
+        Label1.Text = num1.ToString()
+    End Sub
+
+    Private Sub increamentVariable(ByRef x As Integer)
+        x += 5
+
+    End Sub
+
 
 End Class
