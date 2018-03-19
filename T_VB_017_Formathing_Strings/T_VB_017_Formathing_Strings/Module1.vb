6c645d38fca7cf597467121abf411a95b80e5c66
john <jm.kagaya@gmail.com>
2018-03-19 21:54:47 +0900

    Done
    Done

--- T_VB_017_Formathing_Strings/T_VB_017_Formathing_Strings/Module1.vb
+++ T_VB_017_Formathing_Strings/T_VB_017_Formathing_Strings/Module1.vb
@@ -2,6 +2,16 @@
 
     Sub Main()
 
+        Dim mystring As String = Nothing
+        Console.WriteLine("Please enter a string")
+        mystring = Console.ReadLine()
+        Console.WriteLine("Please enter a decimal value ")
+        Dim mydouble As Double = Console.ReadLine()
+        Console.WriteLine()
+        Console.WriteLine(String.Format("{0:n2}", mydouble))
+        Console.WriteLine(mystring.ToUpper())
+        Console.ReadLine()
+
     End Sub
 
 End Module
