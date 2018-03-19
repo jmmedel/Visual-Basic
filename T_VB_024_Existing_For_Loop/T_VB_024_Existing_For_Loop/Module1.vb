583e7c495378a9e514ab4ecbf34e3078f9393256
john <jm.kagaya@gmail.com>
2018-03-19 22:42:35 +0900

    Done
    Done

--- T_VB_024_Existing_For_Loop/T_VB_024_Existing_For_Loop/Module1.vb
+++ T_VB_024_Existing_For_Loop/T_VB_024_Existing_For_Loop/Module1.vb
@@ -1,7 +1,21 @@
 ﻿Module Module1
 
     Sub Main()
+        Console.WriteLine("Normal for loop")
 
+        For index = 1 To 10
+            Console.WriteLine(index)
+        Next
+        Console.WriteLine()
+        Console.WriteLine("For loop that exits before its finished")
+        For index = 1 To 10
+            Console.WriteLine(index)
+            If index = 4 Then
+                'Exit to the loop '
+                Exit For
+            End If
+        Next
+        Console.ReadLine()
     End Sub
 
 End Module
