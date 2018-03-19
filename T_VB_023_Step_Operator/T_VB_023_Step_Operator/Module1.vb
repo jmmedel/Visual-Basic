c34459a96a352246aae9d32d539bcfc64ca90878
john <jm.kagaya@gmail.com>
2018-03-19 22:33:33 +0900

    Done
    Done

--- T_VB_023_Step_Operator/T_VB_023_Step_Operator/Module1.vb
+++ T_VB_023_Step_Operator/T_VB_023_Step_Operator/Module1.vb
@@ -1,7 +1,22 @@
 ﻿Module Module1
 
     Sub Main()
+        Console.WriteLine("Normal for loop")
+        For index = 1 To 10
+            Console.WriteLine(index)
+        Next
+        Console.WriteLine()
+        Console.WriteLine("Step by 2 ")
+        For index = 1 To 10 Step 2
+            Console.WriteLine(index)
+        Next
+        Console.WriteLine()
+        Console.WriteLine("Step by -4 ")
+        For index = 20 To 1 Step -4
+            Console.WriteLine(index)
+        Next
 
+        Console.ReadLine()
     End Sub
 
 End Module
