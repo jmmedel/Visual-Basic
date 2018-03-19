4463e21c66e916fbcbc1cedfb56cc19eea441e4a
john <jm.kagaya@gmail.com>
2018-03-19 21:44:31 +0900

    Done
    Done

--- T_VB_015_Get_Length_Of_String/T_VB_015_Get_Length_Of_String/Module1.vb
+++ T_VB_015_Get_Length_Of_String/T_VB_015_Get_Length_Of_String/Module1.vb
@@ -1,7 +1,16 @@
 ﻿Module Module1
 
     Sub Main()
+        Dim username As String = Nothing
+        Console.WriteLine("What is your username?")
+        username = Console.ReadLine()
 
+        If username.Length.Equals(10) = 10 Then
+            Console.WriteLine("You have benn granted access")
+        Else
+            Console.WriteLine("your username is the right length")
+        End If
+        Console.ReadLine()
     End Sub
 
 End Module
