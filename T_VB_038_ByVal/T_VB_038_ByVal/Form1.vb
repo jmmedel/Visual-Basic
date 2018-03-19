6dc4fba6a7926b8a1e7b6924781ee32ebd5237d4
john <jm.kagaya@gmail.com>
2018-03-20 00:01:58 +0900

    Done
    Done

--- T_VB_038_ByVal/T_VB_038_ByVal/Form1.vb
+++ T_VB_038_ByVal/T_VB_038_ByVal/Form1.vb
@@ -1,3 +1,10 @@
 ﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        MessageBox.Show(substractnumber(TextBox1.Text, TextBox2.Text))
+    End Sub
+
+    Private Function substractnumber(ByVal num1 As Double, ByVal num2 As Double) As Double
+        Return num1 - num2
+    End Function
 
 End Class
