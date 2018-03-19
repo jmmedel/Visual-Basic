7ed7ac4fb5364697e2018c8c9f90db6c2e0ee99f
john <jm.kagaya@gmail.com>
2018-03-20 01:36:23 +0900

    Done
    Done

--- T_VB_040_Optional_ByVal/T_VB_040_Optional_ByVal/Form1.Designer.vb
+++ T_VB_040_Optional_ByVal/T_VB_040_Optional_ByVal/Form1.Designer.vb
@@ -22,9 +22,51 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.txttiitle = New System.Windows.Forms.TextBox()
+        Me.textmesg = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'txttiitle
+        '
+        Me.txttiitle.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.txttiitle.Location = New System.Drawing.Point(0, 0)
+        Me.txttiitle.Name = "txttiitle"
+        Me.txttiitle.Size = New System.Drawing.Size(284, 19)
+        Me.txttiitle.TabIndex = 0
+        '
+        'textmesg
+        '
+        Me.textmesg.Location = New System.Drawing.Point(0, 26)
+        Me.textmesg.Name = "textmesg"
+        Me.textmesg.Size = New System.Drawing.Size(284, 19)
+        Me.textmesg.TabIndex = 1
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(86, 60)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(105, 23)
+        Me.Button1.TabIndex = 2
+        Me.Button1.Text = "Show_Mesage"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(284, 180)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.textmesg)
+        Me.Controls.Add(Me.txttiitle)
+        Me.Name = "Form1"
         Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
 
+    Friend WithEvents txttiitle As TextBox
+    Friend WithEvents textmesg As TextBox
+    Friend WithEvents Button1 As Button
 End Class
--- T_VB_040_Optional_ByVal/T_VB_040_Optional_ByVal/Form1.vb
+++ T_VB_040_Optional_ByVal/T_VB_040_Optional_ByVal/Form1.vb
@@ -1,3 +1,18 @@
 ﻿Public Class Form1
 
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim msg As String = textmesg.Text
+        Dim tittle As String = Nothing
+        If txttiitle.TextLength > 0 Then
+            tittle = txttiitle.Text
+            showmessage(msg, tittle)
+        End If
+
+    End Sub
+
+    Private Sub showmessage(ByVal msg As String, Optional ByVal tiitle As String = "Efault title ")
+        MessageBox.Show(msg, tiitle)
+
+    End Sub
+
 End Class
