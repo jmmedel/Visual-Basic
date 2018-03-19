18f858ada92d31f74f73a89ae7cc96094eda417f
john <jm.kagaya@gmail.com>
2018-03-19 23:56:30 +0900

    Done
    Done

---
+++ T_VB_037_Functions/T_VB_037_Functions/Form1.vb
@@ -0,0 +1,15 @@
+﻿Public Class Form1
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        Dim answer As Double = solveMath()
+        MessageBox.Show(solveMath())
+    End Sub
+
+    Private Function solveMath() As Double
+        Dim num1 As Integer = 23
+        Dim num2 As Integer = 5
+        Return num1 / num2
+    End Function
+
+End Class
