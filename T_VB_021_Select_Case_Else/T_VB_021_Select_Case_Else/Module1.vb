14c0b5a54db53a61829c2d11209b25e5586f92f8
john <jm.kagaya@gmail.com>
2018-03-19 22:10:29 +0900

    Done
    Done

--- T_VB_021_Select_Case_Else/T_VB_021_Select_Case_Else/Module1.vb
+++ T_VB_021_Select_Case_Else/T_VB_021_Select_Case_Else/Module1.vb
@@ -2,6 +2,20 @@
 
     Sub Main()
 
+        Dim mystring As String = Nothing
+        Console.WriteLine("Please enter a string ")
+        mystring = Console.ReadLine()
+        Select Case mystring
+
+            Case "helo"
+                Console.WriteLine("Goodbye")
+            Case "goodbye"
+                Console.WriteLine("Heelo")
+            Case "fishing"
+                Console.WriteLine("Boat")
+
+        End Select
+        Console.ReadLine()
     End Sub
 
 End Module
