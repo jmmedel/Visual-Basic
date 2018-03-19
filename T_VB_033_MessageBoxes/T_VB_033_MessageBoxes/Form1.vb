6b4a070381fe62b9210d346ffa1753f2a8070790
john <jm.kagaya@gmail.com>
2018-03-19 23:27:43 +0900

    Done
    Done

---
+++ T_VB_033_MessageBoxes/T_VB_033_MessageBoxes/Form1.vb
@@ -0,0 +1,8 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        MessageBox.Show("This is a bare messagebox")
+        MessageBox.Show("This message is has a tiitle", "tiitle")
+        MessageBox.Show("This has an icon,", "tiitle", MessageBoxButtons.OK)
+
+    End Sub
+End Class
