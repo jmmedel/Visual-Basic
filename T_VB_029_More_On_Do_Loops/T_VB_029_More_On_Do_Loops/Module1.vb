44a8bef3d58cdc9932c1adedce42244014c7bf24
john <jm.kagaya@gmail.com>
2018-03-19 23:07:47 +0900

    done
    Done

--- T_VB_029_More_On_Do_Loops/T_VB_029_More_On_Do_Loops/Module1.vb
+++ T_VB_029_More_On_Do_Loops/T_VB_029_More_On_Do_Loops/Module1.vb
@@ -1,7 +1,19 @@
 ﻿Module Module1
 
     Sub Main()
-
+        Console.WriteLine("Normal Do until Loop")
+        Dim num1 As Integer = 0
+        Do Until num1 = 5
+            num1 += 1
+            Console.WriteLine(num1)
+        Loop
+        Console.WriteLine()
+        Console.WriteLine("Do until Variant ")
+        num1 = 10
+        Do
+            Console.WriteLine(num1)
+        Loop Until num1 = 10
+        Console.ReadLine()
     End Sub
 
 End Module
