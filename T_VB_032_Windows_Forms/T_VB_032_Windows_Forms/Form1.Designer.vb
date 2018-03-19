ed751074b76197c32c97d667a8b460291ce21f9c
john <jm.kagaya@gmail.com>
2018-03-19 23:20:59 +0900

    Done
    Done

---
+++ T_VB_032_Windows_Forms/T_VB_032_Windows_Forms/Form1.Designer.vb
@@ -0,0 +1,52 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btm1 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'btm1
+        '
+        Me.btm1.Location = New System.Drawing.Point(105, 73)
+        Me.btm1.Name = "btm1"
+        Me.btm1.Size = New System.Drawing.Size(75, 23)
+        Me.btm1.TabIndex = 0
+        Me.btm1.Text = "Heelo"
+        Me.btm1.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.ActiveCaption
+        Me.ClientSize = New System.Drawing.Size(284, 201)
+        Me.Controls.Add(Me.btm1)
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Client"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents btm1 As Button
+End Class
---
+++ T_VB_032_Windows_Forms/T_VB_032_Windows_Forms/Form1.vb
@@ -0,0 +1,5 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles btm1.Click
+        MessageBox.Show("Heelo", "MSG")
+    End Sub
+End Class
