06c092ed16bdbc534c8f96a0c2a7c74bfdf6bfc6
john <jm.kagaya@gmail.com>
2018-03-19 23:02:29 +0900

    Done
    Done

--- T_VB_028_Exit_Do_Loops/T_VB_028_Exit_Do_Loops/Module1.vb
+++ T_VB_028_Exit_Do_Loops/T_VB_028_Exit_Do_Loops/Module1.vb
@@ -1,6 +1,13 @@
 ﻿Module Module1
 
     Sub Main()
+        Dim num1 As Integer = 0
+        Do Until num1 = 10
+            If num1 = 5 Then Exit Do
+            Console.WriteLine(num1)
+            num1 += 1
+        Loop
+        Console.ReadLine()
 
     End Sub
 
