07d7d61e649ff162b4a775f50daececa14fed9c7
john <jm.kagaya@gmail.com>
2018-03-19 23:37:15 +0900

    Done
    Done

--- T_VB_034_MessageBox_Input/T_VB_034_MessageBox_Input/Form1.vb
+++ T_VB_034_MessageBox_Input/T_VB_034_MessageBox_Input/Form1.vb
@@ -1,3 +1,12 @@
 ﻿Public Class Form1
 
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If MessageBox.Show("Click something", "Tiitle", MessageBoxButtons.AbortRetryIgnore, MessageBoxIcon.Question) = Windows.Forms.DialogResult.Abort Then
+            MessageBox.Show("Aborted")
+        End If
+
+
+
+    End Sub
+
 End Class
