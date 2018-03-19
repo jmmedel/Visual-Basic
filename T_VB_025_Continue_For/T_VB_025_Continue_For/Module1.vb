ed15e9cac54c2a4fbfe24be6b9bdcc27eed2b917
john <jm.kagaya@gmail.com>
2018-03-19 22:48:07 +0900

    Done
    Done

--- T_VB_025_Continue_For/T_VB_025_Continue_For/Module1.vb
+++ T_VB_025_Continue_For/T_VB_025_Continue_For/Module1.vb
@@ -1,7 +1,17 @@
 ﻿Module Module1
 
     Sub Main()
-
+        Console.WriteLine("Normal for loop")
+        For index = 1 To 10
+            Console.WriteLine(index)
+        Next
+        Console.WriteLine()
+        Console.WriteLine("Contiue Statement")
+        For index = 1 To 10
+            If index = 5 Then Continue For
+            Console.WriteLine(index)
+        Next
+        Console.ReadLine()
     End Sub
 
 End Module
