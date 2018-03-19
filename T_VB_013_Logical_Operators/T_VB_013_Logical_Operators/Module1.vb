0069113d8696b16b1990c49ffd6a86c6d0b02dfd
john <jm.kagaya@gmail.com>
2018-03-19 21:26:38 +0900

    Done
    Done

--- T_VB_013_Logical_Operators/T_VB_013_Logical_Operators/Module1.vb
+++ T_VB_013_Logical_Operators/T_VB_013_Logical_Operators/Module1.vb
@@ -2,6 +2,18 @@
 
     Sub Main()
 
+        Dim username As String = Nothing
+        Dim password As String = Nothing
+        Console.WriteLine("What is your name?")
+        username = Console.ReadLine()
+        Console.WriteLine("What is your password")
+        password = Console.ReadLine()
+        If username = "Kagaya " Or username <> Nothing Then
+            Console.WriteLine("Heelo " & username)
+        End If
+
+        Console.ReadLine()
+
     End Sub
 
 End Module
