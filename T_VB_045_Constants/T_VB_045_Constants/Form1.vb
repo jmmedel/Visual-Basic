9e9b21cf7e599092c225f0d426dcd21ac764d2c5
john <jm.kagaya@gmail.com>
2018-03-20 10:06:23 +0900

    Done
    Done

--- T_VB_045_Constants/T_VB_045_Constants/Form1.vb
+++ T_VB_045_Constants/T_VB_045_Constants/Form1.vb
@@ -3,4 +3,20 @@
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
     End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        MessageBox.Show(Quater_Value)
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        MessageBox.Show(Quater_Value)
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        MessageBox.Show(Quater_Value)
+    End Sub
+
+    Private Const Quater_Value As Double = 0.25
+
+
 End Class
