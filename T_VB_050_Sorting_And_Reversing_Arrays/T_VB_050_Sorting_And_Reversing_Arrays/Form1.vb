b5e4288cd9198dcebafaadcc62617b53f0f80765
john <jm.kagaya@gmail.com>
2018-03-20 23:18:48 +0900

    Done
    Done

--- T_VB_050_Sorting_And_Reversing_Arrays/T_VB_050_Sorting_And_Reversing_Arrays/Form1.vb
+++ T_VB_050_Sorting_And_Reversing_Arrays/T_VB_050_Sorting_And_Reversing_Arrays/Form1.vb
@@ -5,37 +5,38 @@
         listArray.Items.Clear()
     End Sub
 
-    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-        SubclearList()
-
-
-        stringclasses(0) = "Web Design"
-        stringclasses(1) = "Spanish"
-        stringclasses(2) = "English"
-        stringclasses(3) = "Chemistry"
-        stringclasses(4) = "History"
-        stringclasses(5) = "Calculus"
-
-        For Each item As String In stringclasses
+    Private Sub AdditemtoList(ByVal argArray() As String)
+        'Enumerate Array'
+        For Each item As String In argArray
             listArray.Items.Add(item)
         Next
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        SubclearList()
+        AdditemtoList(stringclasses)
     End Sub
 
     Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
         SubclearList()
 
         Array.Sort(stringclasses)
-        For Each item As String In stringclasses
-            listArray.Items.Add(stringclasses(item))
-        Next
+        AdditemtoList(stringclasses)
 
     End Sub
 
     Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
         SubclearList()
         Array.Reverse(stringclasses)
-        For num As Integer = 0 To stringclasses.Length - 1
-            listArray.Items.Add(stringclasses(num))
-        Next
+        AdditemtoList(stringclasses)
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        stringclasses(0) = "Web Design"
+        stringclasses(1) = "Spanish"
+        stringclasses(2) = "English"
+        stringclasses(3) = "Chemistry"
+        stringclasses(4) = "History"
+        stringclasses(5) = "Calculus"
     End Sub
 End Class
