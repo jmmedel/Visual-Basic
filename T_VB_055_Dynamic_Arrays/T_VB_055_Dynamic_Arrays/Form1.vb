2a42a28aa81bbe25e2db1e20f93050818e3ad34f
john <jm.kagaya@gmail.com>
2018-03-21 20:14:26 +0900

    Done
    Done

--- T_VB_055_Dynamic_Arrays/T_VB_055_Dynamic_Arrays/Form1.vb
+++ T_VB_055_Dynamic_Arrays/T_VB_055_Dynamic_Arrays/Form1.vb
@@ -1,13 +1,13 @@
 ﻿Public Class Form1
 
-    Private strSports() As String = {"baseball", "Baseketball", "Football"}
+    Private strSports() As String = {"baseball", "Baseketball", "Football", "Wakeboqwoeoqwe", "QWEASDZXC"}
 
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
         For Each Item As String In strSports
             ListBox1.Items.Add("[" & Item & "]")
         Next
 
-        ReDim strSports(4)
+        ReDim Preserve strSports(4)
         strSports(3) = "wakeboarding"
         strSports(4) = "soccer"
 
---
+++ T_VB_056_ArrayList/T_VB_056_ArrayList/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("T_VB_056_ArrayList")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("T_VB_056_ArrayList")>
+<Assembly: AssemblyCopyright("Copyright ©  2018")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("4d218f6d-c27f-4730-8617-f5dd5d7f3d44")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
