55ca49fe1d282458ef6f210baa79578c7c0482c3
john <jm.kagaya@gmail.com>
2018-03-21 10:53:20 +0900

    Done
    Done

--- T_VB_051_Enumerations/T_VB_051_Enumerations/Form1.vb
+++ T_VB_051_Enumerations/T_VB_051_Enumerations/Form1.vb
@@ -1,3 +1,16 @@
 ﻿Public Class Form1
 
+    Private Enum DayAction As Integer
+        Awake = 0
+        Asleep = 1
+        coding = 2
+    End Enum
+
+    Private action As DayAction = DayAction.Asleep
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        MessageBox.Show(action.ToString())
+
+    End Sub
+
 End Class
