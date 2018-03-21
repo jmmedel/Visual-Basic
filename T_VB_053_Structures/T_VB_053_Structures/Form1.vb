f9f7075098f1ce6fcf16d8785254e16aa9fcbe24
john <jm.kagaya@gmail.com>
2018-03-21 14:18:43 +0900

    Done
    Done

--- T_VB_053_Structures/T_VB_053_Structures/Form1.vb
+++ T_VB_053_Structures/T_VB_053_Structures/Form1.vb
@@ -9,9 +9,25 @@
 
     End Structure
 
+    Private objcustomer As Customer
+
+
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
     End Sub
 
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        objcustomer.Firstname = "Jim"
+        objcustomer.lastname = "Jones"
+        objcustomer.Email = "jm_sasuke25@yahoo.com"
+        DisplayCustomer(objcustomer)
+    End Sub
+
+    Private Sub DisplayCustomer(ByVal cust As Customer)
+        txtFirstname.Text = cust.Firstname
+        txtLastName.Text = cust.lastname
+        txtEmail.Text = cust.Email
+    End Sub
 
 End Class
