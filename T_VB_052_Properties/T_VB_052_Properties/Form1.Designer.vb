0ee4768ff378b4f2d614f2b71fce1aeaa456fc55
john <jm.kagaya@gmail.com>
2018-03-21 11:47:13 +0900

    done

--- T_VB_052_Properties/T_VB_052_Properties/Form1.Designer.vb
+++ T_VB_052_Properties/T_VB_052_Properties/Form1.Designer.vb
@@ -22,9 +22,41 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.DateTimePicker1 = New System.Windows.Forms.DateTimePicker()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'DateTimePicker1
+        '
+        Me.DateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Time
+        Me.DateTimePicker1.Location = New System.Drawing.Point(12, 32)
+        Me.DateTimePicker1.Name = "DateTimePicker1"
+        Me.DateTimePicker1.Size = New System.Drawing.Size(200, 19)
+        Me.DateTimePicker1.TabIndex = 0
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(27, 75)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(109, 12)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "State Not Initializing"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(284, 117)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.DateTimePicker1)
+        Me.Name = "Form1"
         Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
 
+    Friend WithEvents DateTimePicker1 As DateTimePicker
+    Friend WithEvents Label1 As Label
 End Class
--- T_VB_052_Properties/T_VB_052_Properties/Form1.vb
+++ T_VB_052_Properties/T_VB_052_Properties/Form1.vb
@@ -1,3 +1,39 @@
 ﻿Public Class Form1
+    Private Enum DayAction As Integer
+        Asleep = 0
+        Wakingup = 1
+        School = 2
+        Lunch = 3
+        Home = 4
+        Programming = 5
 
+    End Enum
+
+
+    Private state As DayAction
+    Private Property Hour() As Integer
+
+        Get
+            Return DateTimePicker1.Value.Hour
+        End Get
+        Set(ByVal value As Integer)
+            DateTimePicker1.Value = New Date(Now.Year, Now.Month, Now.Day, value, 0, 0)
+            If value >= 20 And value < 6 Then
+                state = DayAction.Asleep
+            End If
+            Label1.Text = "At" & value & ":00, Kagaya is "
+        End Set
+
+    End Property
+
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        Me.Hour = Now.Hour
+
+    End Sub
+
+    Private Sub DateTimePicker1_ValueChanged(sender As Object, e As EventArgs) Handles DateTimePicker1.ValueChanged
+        Me.Hour = DateTimePicker1.Value.Hour
+    End Sub
 End Class
