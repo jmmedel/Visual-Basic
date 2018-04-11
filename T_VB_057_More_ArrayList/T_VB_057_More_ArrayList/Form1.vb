8e8ac636464f23d84be19ef9ef3e777c3fcb320b
john <jm.kagaya@gmail.com>
2018-04-12 00:14:23 +0900

    Done
    Done

--- T_VB_057_More_ArrayList/T_VB_057_More_ArrayList/Form1.vb
+++ T_VB_057_More_ArrayList/T_VB_057_More_ArrayList/Form1.vb
@@ -1,20 +1,20 @@
 ﻿Public Class Form1
-
+    '82'
     Private myarraylist As New ArrayList
-    Private myarrat() As String = {"Sam", "Kagaya", "Youtube"}
-
+    Private myarrat() As String = {"Sam", "kagaya", "Youtube"}
+    Private index As Integer
 
 
     'This is form load this is where its load'
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-
+        myarraylist.TrimToSize()
+        myarraylist.Capacity = 5
         myarraylist.Add(10)
         myarraylist.Add("Heeloo")
         myarraylist.Add(1.34)
         myarraylist.Add(20)
         myarraylist.Add("Bye")
         myarraylist.Add(True)
-        myarraylist.Add("Kagaya")
         myarraylist.Add(True)
     End Sub
 
@@ -22,15 +22,17 @@
 
     'This is where it click '
     Private Sub Show_Array_Click(sender As Object, e As EventArgs) Handles Show_Array.Click
+
         myarraylist.Remove(True)
         myarraylist.RemoveAt(1)
+        'join array '
         myarraylist.AddRange(myarrat)
         For Each item As String In myarraylist
             ListBox1.Items.Add(item.ToString())
 
         Next
-
-
+        MsgBox(myarraylist.Count.ToString())
+        index = myarraylist.BinarySearch("Kagaya")
 
     End Sub
 
