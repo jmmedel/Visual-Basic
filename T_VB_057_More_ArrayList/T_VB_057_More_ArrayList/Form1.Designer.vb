ca5878f907329ec692084401ef2a4cd5e565eb2a
john <jm.kagaya@gmail.com>
2018-04-11 22:17:36 +0900

    Done
    Done

--- T_VB_057_More_ArrayList/T_VB_057_More_ArrayList/Form1.Designer.vb
+++ T_VB_057_More_ArrayList/T_VB_057_More_ArrayList/Form1.Designer.vb
@@ -22,9 +22,41 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.Show_Array = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.ItemHeight = 12
+        Me.ListBox1.Location = New System.Drawing.Point(22, 23)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(296, 172)
+        Me.ListBox1.TabIndex = 0
+        '
+        'Show_Array
+        '
+        Me.Show_Array.Location = New System.Drawing.Point(22, 216)
+        Me.Show_Array.Name = "Show_Array"
+        Me.Show_Array.Size = New System.Drawing.Size(296, 23)
+        Me.Show_Array.TabIndex = 1
+        Me.Show_Array.Text = "Button1"
+        Me.Show_Array.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(330, 260)
+        Me.Controls.Add(Me.Show_Array)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Name = "Form1"
         Me.Text = "Form1"
+        Me.ResumeLayout(False)
+
     End Sub
 
+    Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents Show_Array As Button
 End Class
