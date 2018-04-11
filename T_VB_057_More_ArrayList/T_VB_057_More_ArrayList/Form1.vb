014b91a720ce3b2035ef2866cc0694a5334958df
john <jm.kagaya@gmail.com>
2018-04-11 22:45:46 +0900

    Done
    Done

--- T_VB_057_More_ArrayList/T_VB_057_More_ArrayList/Form1.vb
+++ T_VB_057_More_ArrayList/T_VB_057_More_ArrayList/Form1.vb
@@ -1,7 +1,7 @@
 ﻿Public Class Form1
 
     Private myarraylist As New ArrayList
-
+    Private myarrat() As String = {"Sam", "Kagaya", "Youtube"}
 
 
 
@@ -11,17 +11,27 @@
         myarraylist.Add(10)
         myarraylist.Add("Heeloo")
         myarraylist.Add(1.34)
-
+        myarraylist.Add(20)
+        myarraylist.Add("Bye")
+        myarraylist.Add(True)
+        myarraylist.Add("Kagaya")
+        myarraylist.Add(True)
     End Sub
 
 
 
     'This is where it click '
     Private Sub Show_Array_Click(sender As Object, e As EventArgs) Handles Show_Array.Click
-
+        myarraylist.Remove(True)
+        myarraylist.RemoveAt(1)
+        myarraylist.AddRange(myarrat)
         For Each item As String In myarraylist
             ListBox1.Items.Add(item.ToString())
+
         Next
 
+
+
     End Sub
+
 End Class
