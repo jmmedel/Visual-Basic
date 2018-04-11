d45115a4389eee51d71a926de0218886956a5dba
john <jm.kagaya@gmail.com>
2018-04-11 22:30:24 +0900

    Done
    Done

--- T_VB_057_More_ArrayList/T_VB_057_More_ArrayList/Form1.vb
+++ T_VB_057_More_ArrayList/T_VB_057_More_ArrayList/Form1.vb
@@ -1,3 +1,27 @@
 ﻿Public Class Form1
 
+    Private myarraylist As New ArrayList
+
+
+
+
+    'This is form load this is where its load'
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        myarraylist.Add(10)
+        myarraylist.Add("Heeloo")
+        myarraylist.Add(1.34)
+
+    End Sub
+
+
+
+    'This is where it click '
+    Private Sub Show_Array_Click(sender As Object, e As EventArgs) Handles Show_Array.Click
+
+        For Each item As String In myarraylist
+            ListBox1.Items.Add(item.ToString())
+        Next
+
+    End Sub
 End Class
