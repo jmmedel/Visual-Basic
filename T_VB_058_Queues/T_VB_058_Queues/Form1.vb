96f9c1c139f10566ba453f9785a929b6bcc5a3e1
john <jm.kagaya@gmail.com>
2018-04-14 11:27:16 +0900

    Done
    Done

--- T_VB_058_Queues/T_VB_058_Queues/Form1.vb
+++ T_VB_058_Queues/T_VB_058_Queues/Form1.vb
@@ -2,5 +2,14 @@
     '83'
     Private q As New Queue
 
-
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        q.Enqueue(1)
+        q.Enqueue(99)
+        q.Enqueue(25)
+        q.Enqueue(31)
+        q.Enqueue(22)
+        q.Dequeue()
+        MsgBox(q.Dequeue().ToString())
+        MsgBox(q.Peek().ToString())
+    End Sub
 End Class
