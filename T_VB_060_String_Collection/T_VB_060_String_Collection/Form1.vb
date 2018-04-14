420a4090170bdca95e9324d3bf5dc0185e4582e7
john <jm.kagaya@gmail.com>
2018-04-14 12:36:42 +0900

    Done
    done

--- T_VB_060_String_Collection/T_VB_060_String_Collection/Form1.vb
+++ T_VB_060_String_Collection/T_VB_060_String_Collection/Form1.vb
@@ -1,3 +1,24 @@
-﻿Public Class Form1
+﻿Imports System.Collections.Specialized
+
+Public Class Form1
+
+
+    Private stringcol As New StringCollection
+    Private myArray() As String = {"Joe", "Kagaya", "john"}
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        stringcol.Add("kagaya")
+        'Add aray'
+        stringcol.AddRange(myArray)
+        stringcol.Remove("john")
+        stringcol.RemoveAt(0)
+        Dim isthere As Boolean = stringcol.Contains("john")
+        For Each item As String In stringcol
+            ListBox1.Items.Add(item)
+
+        Next
+        MsgBox(isthere.ToString())
+    End Sub
+
 
 End Class
