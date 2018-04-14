bbaafbe46106f88d6fad04400a005f393de782e1
john <jm.kagaya@gmail.com>
2018-04-14 14:06:08 +0900

    Done
    done

--- T_VB_061_Date/T_VB_061_Date/Form1.vb
+++ T_VB_061_Date/T_VB_061_Date/Form1.vb
@@ -1,6 +1,12 @@
 ﻿Public Class Form1
-
+    '87'
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim currentDate As Date
+        currentDate = Now
+
+        MessageBox.Show(currentDate.ToString(), "Date Application")
+        MsgBox(currentDate.ToLongDateString())
+
 
     End Sub
 
