f1812aedeb038a622678d80ec172190109074462
john <jm.kagaya@gmail.com>
2018-04-14 11:55:14 +0900

    Done
    done

--- T_VB_059_Stacks/T_VB_059_Stacks/Form1.vb
+++ T_VB_059_Stacks/T_VB_059_Stacks/Form1.vb
@@ -1,3 +1,18 @@
 ﻿Public Class Form1
 
+    Private mystack As New Stack
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        mystack.Push(1)
+        mystack.Push(21)
+        mystack.Push(31)
+        mystack.Pop()
+        MsgBox(mystack.Pop().ToString())
+        MsgBox(mystack.Peek().ToString())
+    End Sub
+
+
+
+
+
 End Class
