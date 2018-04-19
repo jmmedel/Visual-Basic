94a6bc511fc39de358d7b78642b66e1ae8119fdf
kagaya25 <32299866+kagaya25@users.noreply.github.com>
2018-04-20 01:50:49 +0900

    Done
    Done

---
+++ VB_Sandbox/Sandbox1/Lesson18Delegates/Lesson18Delegates/Lesson18Delegates/Form1.vb
@@ -0,0 +1,12 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles Me.Load
+        AddHandler DataGridView1.CellContentClick, AddressOf CellClick
+    End Sub
+
+    Private Sub CellClick(s As Object, e As DataGridViewCellEventArgs)
+        Dim message = $"Column Index: {e.ColumnIndex}" + vbCr + $"Row Index: {e.RowIndex}"
+        MessageBox.Show(message)
+    End Sub
+
+
+End Class
