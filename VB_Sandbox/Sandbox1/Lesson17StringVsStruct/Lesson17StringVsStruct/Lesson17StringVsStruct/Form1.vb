56ee554379f1f6b385f964194dc0bc7716f8a2f5
kagaya25 <32299866+kagaya25@users.noreply.github.com>
2018-04-20 01:47:59 +0900

    Done
    Done

---
+++ VB_Sandbox/Sandbox1/Lesson17StringVsStruct/Lesson17StringVsStruct/Lesson17StringVsStruct/Form1.vb
@@ -0,0 +1,8 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles Me.Load
+        Dim s1 As String = "hello 1"
+        Dim s2 = s1
+        s1 = "hello 2"
+        s1 = "hello 3"
+    End Sub
+End Class
---
+++ VB_Sandbox/Sandbox1/Lesson17StringVsStruct/Lesson17StringVsStruct/Lesson17StringVsStruct/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("Lesson17StringVsStruct")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("Lesson17StringVsStruct")>
+<Assembly: AssemblyCopyright("Copyright ©  2018")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("35794a4c-9e24-4d2b-8842-1cf2c7e68987")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
