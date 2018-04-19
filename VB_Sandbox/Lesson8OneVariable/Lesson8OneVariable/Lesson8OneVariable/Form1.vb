84c969611502acac5dd95d62f0d7017af64a52e9
kagaya25 <32299866+kagaya25@users.noreply.github.com>
2018-04-20 00:10:08 +0900

    Done
    Done

---
+++ VB_Sandbox/Lesson8OneVariable/Lesson8OneVariable/Lesson8OneVariable/Form1.vb
@@ -0,0 +1,6 @@
+﻿Public Class Form1
+    Private Sub TrackBar1_Scroll(sender As Object, e As EventArgs) Handles TrackBar1.Scroll
+        'Dim value As Integer = TrackBar1.Value
+        Label1.Text = $"Value:{TrackBar1.Value }"
+    End Sub
+End Class
