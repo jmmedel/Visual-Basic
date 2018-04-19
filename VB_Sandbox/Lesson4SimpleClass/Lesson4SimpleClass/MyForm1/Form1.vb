7af9ea57f8fe89d4e76d5b3deacb6fa93d866d34
kagaya25 <32299866+kagaya25@users.noreply.github.com>
2018-04-19 22:53:15 +0900

    Done
    Done

---
+++ VB_Sandbox/Lesson4SimpleClass/Lesson4SimpleClass/MyForm1/Form1.vb
@@ -0,0 +1,17 @@
+﻿Public Class Form1
+    Dim t As ToolTip = New ToolTip()
+
+    Private Sub TrackBar1_Scroll(sender As Object, e As EventArgs) Handles TrackBar1.Scroll
+        t.SetToolTip(TrackBar1, TrackBar1.Value.ToString())
+    End Sub
+
+    Private Sub TrackBar1_Paint(sender As Object, e As PaintEventArgs) Handles TrackBar1.Paint
+
+    End Sub
+End Class
+
+
+
+'Public Class Form2
+
+'End Class
