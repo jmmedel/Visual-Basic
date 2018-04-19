1eedad9bb33c8b54fe7211536f39a2a48a6b96a9
kagaya25 <32299866+kagaya25@users.noreply.github.com>
2018-04-20 01:23:16 +0900

    Done
    Done

---
+++ VB_Sandbox/Lessons14and15and16ImportNameSpace/Lesson14ImportNameSpace/Lesson14ImportNameSpace/Form1.vb
@@ -0,0 +1,7 @@
+﻿Public Class Form1
+    Private Sub PictureBox1_Click(sender As Object, e As EventArgs) Handles PictureBox1.Click
+        Dim path = IO.Path.GetFileName(PictureBox1.ImageLocation)
+        MessageBox.Show(path, "Picture", MessageBoxButtons.OK, MessageBoxIcon.Information)
+    End Sub
+End Class
+
