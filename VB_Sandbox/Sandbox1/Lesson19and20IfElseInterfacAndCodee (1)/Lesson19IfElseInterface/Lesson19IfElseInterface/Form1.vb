1ce60d08907c40b3b57df842c0394c19f651a469
kagaya25 <32299866+kagaya25@users.noreply.github.com>
2018-04-21 03:49:44 +0900

    Done
    Done

---
+++ VB_Sandbox/Sandbox1/Lesson19and20IfElseInterfacAndCodee (1)/Lesson19IfElseInterface/Lesson19IfElseInterface/Form1.vb
@@ -0,0 +1,10 @@
+﻿Public Class Form1
+    Private Sub chkCalendar_CheckedChanged(sender As Object, e As EventArgs) Handles chkCalendar.CheckedChanged
+        If chkCalendar.Checked Then
+            MonthCalendar1.Visible = True
+        Else
+            MonthCalendar1.Visible = False
+        End If
+
+    End Sub
+End Class
---
+++ VB_Sandbox/Sandbox1/Lesson19and20IfElseInterfacAndCodee/Lesson19IfElseInterface/Lesson19IfElseInterface/Form1.Designer.vb
@@ -0,0 +1,63 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.chkCalendar = New System.Windows.Forms.CheckBox()
+        Me.MonthCalendar1 = New System.Windows.Forms.MonthCalendar()
+        Me.SuspendLayout()
+        '
+        'chkCalendar
+        '
+        Me.chkCalendar.AutoSize = True
+        Me.chkCalendar.Font = New System.Drawing.Font("Microsoft Sans Serif", 40.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.chkCalendar.Location = New System.Drawing.Point(13, 13)
+        Me.chkCalendar.Name = "chkCalendar"
+        Me.chkCalendar.Size = New System.Drawing.Size(266, 67)
+        Me.chkCalendar.TabIndex = 0
+        Me.chkCalendar.Text = "Calendar"
+        Me.chkCalendar.UseVisualStyleBackColor = True
+        '
+        'MonthCalendar1
+        '
+        Me.MonthCalendar1.Location = New System.Drawing.Point(13, 87)
+        Me.MonthCalendar1.Name = "MonthCalendar1"
+        Me.MonthCalendar1.TabIndex = 1
+        Me.MonthCalendar1.Visible = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.MonthCalendar1)
+        Me.Controls.Add(Me.chkCalendar)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents chkCalendar As CheckBox
+    Friend WithEvents MonthCalendar1 As MonthCalendar
+End Class
