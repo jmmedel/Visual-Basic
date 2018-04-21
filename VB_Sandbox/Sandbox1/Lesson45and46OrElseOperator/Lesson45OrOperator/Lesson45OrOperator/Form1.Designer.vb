45eaa9f7f2adbf4c6238f4249e4c3c07816685be
kagaya25 <32299866+kagaya25@users.noreply.github.com>
2018-04-21 12:48:54 +0900

    Done
    Done

---
+++ VB_Sandbox/Sandbox1/Lesson45and46OrElseOperator/Lesson45OrOperator/Lesson45OrOperator/Form1.Designer.vb
@@ -0,0 +1,75 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.txtInput = New System.Windows.Forms.TextBox()
+        Me.lblCount = New System.Windows.Forms.Label()
+        Me.btnCount = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'txtInput
+        '
+        Me.txtInput.Font = New System.Drawing.Font("Microsoft Sans Serif", 40.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtInput.Location = New System.Drawing.Point(13, 13)
+        Me.txtInput.Name = "txtInput"
+        Me.txtInput.Size = New System.Drawing.Size(775, 68)
+        Me.txtInput.TabIndex = 0
+        '
+        'lblCount
+        '
+        Me.lblCount.AutoSize = True
+        Me.lblCount.Font = New System.Drawing.Font("Microsoft Sans Serif", 40.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblCount.Location = New System.Drawing.Point(12, 96)
+        Me.lblCount.Name = "lblCount"
+        Me.lblCount.Size = New System.Drawing.Size(0, 63)
+        Me.lblCount.TabIndex = 1
+        '
+        'btnCount
+        '
+        Me.btnCount.Font = New System.Drawing.Font("Microsoft Sans Serif", 40.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnCount.Location = New System.Drawing.Point(13, 173)
+        Me.btnCount.Name = "btnCount"
+        Me.btnCount.Size = New System.Drawing.Size(201, 74)
+        Me.btnCount.TabIndex = 2
+        Me.btnCount.Text = "Count"
+        Me.btnCount.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.btnCount)
+        Me.Controls.Add(Me.lblCount)
+        Me.Controls.Add(Me.txtInput)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents txtInput As TextBox
+    Friend WithEvents lblCount As Label
+    Friend WithEvents btnCount As Button
+End Class
---
+++ VB_Sandbox/Sandbox1/Lesson45and46OrElseOperator/Lesson45OrOperator/Lesson45OrOperator/Form1.vb
@@ -0,0 +1,13 @@
+﻿Public Class Form1
+    Private Sub btnCount_Click(sender As Object, e As EventArgs) Handles btnCount.Click
+        Dim text = txtInput.Text.ToLower()
+        Dim count As Integer
+        'This is the moment that counts.
+        For Each c In text
+            If c = "a" OrElse c = "e" OrElse c = "i" OrElse c = "o" OrElse c = "u" Then
+                count += 1
+            End If
+        Next
+        lblCount.Text = $"Count: {count}"
+    End Sub
+End Class
