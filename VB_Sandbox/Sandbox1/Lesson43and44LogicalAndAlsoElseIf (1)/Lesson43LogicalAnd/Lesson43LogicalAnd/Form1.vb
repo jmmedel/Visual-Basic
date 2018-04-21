22ceb17b2c2733d9bb521778f85e532590624f56
kagaya25 <32299866+kagaya25@users.noreply.github.com>
2018-04-21 12:44:48 +0900

    Done
    Done

---
+++ VB_Sandbox/Sandbox1/Lesson43and44LogicalAndAlsoElseIf (1)/Lesson43LogicalAnd/Lesson43LogicalAnd/Form1.vb
@@ -0,0 +1,13 @@
+﻿Public Class Form1
+    Private Sub btnCheck_Click(sender As Object, e As EventArgs) Handles btnCheck.Click
+        If chkDiet.Checked AndAlso chkExercise.Checked Then
+            lblMessage.Text = "Discount: 10%"
+        ElseIf chkDiet.Checked Then
+            lblMessage.Text = "Discount: 5%"
+        ElseIf chkExercise.Checked Then
+            lblMessage.Text = "Discount: 6%"
+        Else
+            lblMessage.Text = "No Discount"
+        End If
+    End Sub
+End Class
---
+++ VB_Sandbox/Sandbox1/Lesson43and44LogicalAndAlsoElseIf/Lesson43LogicalAnd/Lesson43LogicalAnd/Form1.Designer.vb
@@ -0,0 +1,92 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.chkDiet = New System.Windows.Forms.CheckBox()
+        Me.chkExercise = New System.Windows.Forms.CheckBox()
+        Me.btnCheck = New System.Windows.Forms.Button()
+        Me.lblMessage = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'chkDiet
+        '
+        Me.chkDiet.AutoSize = True
+        Me.chkDiet.Font = New System.Drawing.Font("Microsoft Sans Serif", 40.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.chkDiet.Location = New System.Drawing.Point(13, 13)
+        Me.chkDiet.Name = "chkDiet"
+        Me.chkDiet.Size = New System.Drawing.Size(143, 67)
+        Me.chkDiet.TabIndex = 0
+        Me.chkDiet.Text = "Diet"
+        Me.chkDiet.UseVisualStyleBackColor = True
+        '
+        'chkExercise
+        '
+        Me.chkExercise.AutoSize = True
+        Me.chkExercise.Font = New System.Drawing.Font("Microsoft Sans Serif", 40.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.chkExercise.Location = New System.Drawing.Point(361, 13)
+        Me.chkExercise.Name = "chkExercise"
+        Me.chkExercise.Size = New System.Drawing.Size(254, 67)
+        Me.chkExercise.TabIndex = 1
+        Me.chkExercise.Text = "Exercise"
+        Me.chkExercise.UseVisualStyleBackColor = True
+        '
+        'btnCheck
+        '
+        Me.btnCheck.Font = New System.Drawing.Font("Microsoft Sans Serif", 40.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnCheck.Location = New System.Drawing.Point(12, 105)
+        Me.btnCheck.Name = "btnCheck"
+        Me.btnCheck.Size = New System.Drawing.Size(247, 70)
+        Me.btnCheck.TabIndex = 2
+        Me.btnCheck.Text = "Check"
+        Me.btnCheck.UseVisualStyleBackColor = True
+        '
+        'lblMessage
+        '
+        Me.lblMessage.AutoSize = True
+        Me.lblMessage.Font = New System.Drawing.Font("Microsoft Sans Serif", 40.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblMessage.Location = New System.Drawing.Point(12, 201)
+        Me.lblMessage.Name = "lblMessage"
+        Me.lblMessage.Size = New System.Drawing.Size(0, 63)
+        Me.lblMessage.TabIndex = 3
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.lblMessage)
+        Me.Controls.Add(Me.btnCheck)
+        Me.Controls.Add(Me.chkExercise)
+        Me.Controls.Add(Me.chkDiet)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents chkDiet As CheckBox
+    Friend WithEvents chkExercise As CheckBox
+    Friend WithEvents btnCheck As Button
+    Friend WithEvents lblMessage As Label
+End Class
