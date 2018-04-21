c3b4d1d5b2e636a31fda2abbed29b2ba5e3d8368
kagaya25 <32299866+kagaya25@users.noreply.github.com>
2018-04-21 12:41:42 +0900

    Done
    Done

---
+++ VB_Sandbox/Sandbox1/Lesson40_4142Graphics/Lesson40SelectCasePart1/Lesson40SelectCasePart1/Form1.Designer.vb
@@ -0,0 +1,74 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    '<System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.TrackBar1 = New System.Windows.Forms.TrackBar()
+        Me.ToolTip1 = New System.Windows.Forms.ToolTip(Me.components)
+        CType(Me.TrackBar1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 25.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Items.AddRange(New Object() {"Circle", "Square"})
+        Me.ComboBox1.Location = New System.Drawing.Point(13, 13)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(253, 46)
+        Me.ComboBox1.TabIndex = 0
+        '
+        'TrackBar1
+        '
+        Me.TrackBar1.Location = New System.Drawing.Point(273, 13)
+        Me.TrackBar1.Maximum = 200
+        Me.TrackBar1.Minimum = 1
+        Me.TrackBar1.Name = "TrackBar1"
+        Me.TrackBar1.Size = New System.Drawing.Size(515, 45)
+        Me.TrackBar1.TabIndex = 1
+        Me.TrackBar1.Value = 1
+        '
+        'ToolTip1
+        '
+        Me.ToolTip1.IsBalloon = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 617)
+        Me.Controls.Add(Me.TrackBar1)
+        Me.Controls.Add(Me.ComboBox1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.TrackBar1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents TrackBar1 As TrackBar
+    Friend WithEvents ToolTip1 As ToolTip
+End Class
---
+++ VB_Sandbox/Sandbox1/Lesson40_4142Graphics/Lesson40SelectCasePart1/Lesson40SelectCasePart1/Form1.vb
@@ -0,0 +1,41 @@
+﻿Public Class Form1
+    Dim g As Graphics
+    Dim p As Pen
+    Dim v As Integer
+    Dim c As Color = Color.Red
+    Private Sub TrackBar1_Scroll(sender As Object, e As EventArgs) Handles TrackBar1.Scroll
+        ToolTip1.SetToolTip(TrackBar1, TrackBar1.Value)
+        g = Graphics.FromHwnd(Handle)
+        p = New Pen(c, 5)
+        v = TrackBar1.Value
+        Select Case ComboBox1.SelectedItem
+            Case "Circle"
+                g.Clear(BackColor)
+                g.DrawEllipse(p, 100, 100, v, v)
+            Case "Square"
+                g.Clear(BackColor)
+                g.DrawRectangle(p, 100, 100, v, v)
+        End Select
+        g.Dispose()
+        g = Nothing
+        p.Dispose()
+        p = Nothing
+    End Sub
+
+    Private Sub ComboBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ComboBox1.SelectedIndexChanged
+        g = Graphics.FromHwnd(Handle)
+        p = New Pen(c, 5)
+        Select Case ComboBox1.SelectedItem
+            Case "Circle"
+                g.Clear(BackColor)
+                g.DrawEllipse(p, 100, 100, v, v)
+            Case "Square"
+                g.Clear(BackColor)
+                g.DrawRectangle(p, 100, 100, v, v)
+        End Select
+        g.Dispose()
+        g = Nothing
+        p.Dispose()
+        p = Nothing
+    End Sub
+End Class
