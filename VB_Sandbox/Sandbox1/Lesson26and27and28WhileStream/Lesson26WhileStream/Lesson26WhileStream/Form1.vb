0983c5eb7c81aac4e6be078ab107094fa1b62751
kagaya25 <32299866+kagaya25@users.noreply.github.com>
2018-04-21 11:34:28 +0900

    Done
    Done

---
+++ VB_Sandbox/Sandbox1/Lesson26and27and28WhileStream/Lesson26WhileStream/Lesson26WhileStream/Form1.vb
@@ -0,0 +1,13 @@
+﻿Imports System.IO
+Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If OpenFileDialog1.ShowDialog() = DialogResult.OK Then
+            Dim stream As StreamReader = File.OpenText(OpenFileDialog1.FileName)
+            While stream.Peek() <> -1
+                Label1.Text += vbCr + stream.ReadLine()
+
+            End While
+            stream.Close()
+        End If
+    End Sub
+End Class
