4cb410317b7e7f0663d32630f412655016e6cac4
kagaya25 <32299866+kagaya25@users.noreply.github.com>
2018-04-24 11:52:35 +0900

    Done
    Done

--- T_028_VB_nested_Selet_Case/T_028_VB_nested_Selet_Case/Module1.vb
+++ T_028_VB_nested_Selet_Case/T_028_VB_nested_Selet_Case/Module1.vb
@@ -1,7 +1,23 @@
-﻿Module Module1
+﻿
+'VB.Net -Nested Select Case Statement
+'It Is possible To have a Select statement As part Of the statement sequence Of an outer Select statement. Even If the Case constants Of the inner And outer Select contain common values, no conflicts will arise.
+
+Module Module1
 
     Sub Main()
-
+        'local variable definition
+        Dim a As Integer = 100
+        Dim b As Integer = 200
+        Select Case a
+            Case 100
+                Console.WriteLine("This is part of outer case ")
+                Select Case b
+                    Case 200
+                        Console.WriteLine("This is part of inner case ")
+                End Select
+        End Select
+        Console.WriteLine("Exact value of a is : {0}", a)
+        Console.WriteLine("Exact value of b is : {0}", b)
+        Console.ReadLine()
     End Sub
-
 End Module
