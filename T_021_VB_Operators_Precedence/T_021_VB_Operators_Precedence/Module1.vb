f161d6e4450804f4a803b20381793bca16ff9e39
kagaya25 <32299866+kagaya25@users.noreply.github.com>
2018-04-24 11:32:57 +0900

    Done
    Done

--- T_021_VB_Operators_Precedence/T_021_VB_Operators_Precedence/Module1.vb
+++ T_021_VB_Operators_Precedence/T_021_VB_Operators_Precedence/Module1.vb
@@ -1,7 +1,53 @@
-﻿Module Module1
+﻿
+'VB.Net -Operators Precedence
+'Operator precedence determines the grouping of terms in an expression. This affects how an expression Is evaluated. Certain operators have higher precedence than others; for example, the multiplication operator has higher precedence than the addition operator
+
+'For example, x = 7 + 3 * 2; here, x Is assigned 13, Not 20 because operator * has higher precedence than +, so it first get multiplied with 3*2 And then adds into 7.
+
+'Here, Operators With the highest precedence appear at the top Of the table, those With the lowest appear at the bottom. Within an expression, higher precedence operators will be evaluated first.
+
+'Operator    Precedence
+'Await   Highest
+'Exponentiation(^)
+'    Unary identity And negation(+, -)  
+'Multiplication And floating - point division (*, /)	
+'Integer division(\)    
+'Modulus arithmetic(Mod)    
+'Addition And subtraction(+, -) 
+'Arithmetic bit shift (<<, >>)	
+'All Comparison operators (=, <>, <, <=, >, >=, Is, IsNot, Like, TypeOf...Is)	
+'Negation (Not)	
+'Conjunction (And, AndAlso)	
+'Inclusive disjunction (Or, OrElse)	
+'Exclusive disjunction (Xor)	Lowest
+
+
+Module Module1
 
     Sub Main()
-
+        Dim a As Integer = 20
+        Dim b As Integer = 10
+        Dim c As Integer = 15
+        Dim d As Integer = 5
+        Dim e As Integer
+        e = (a + b) * c / d      ' ( 30 * 15 ) / 5
+        Console.WriteLine("Value of (a + b) * c / d is : {0}", e)
+        e = ((a + b) * c) / d    ' (30 * 15 ) / 5
+        Console.WriteLine("Value of ((a + b) * c) / d is  : {0}", e)
+        e = (a + b) * (c / d)   ' (30) * (15/5)
+        Console.WriteLine("Value of (a + b) * (c / d) is  : {0}", e)
+        e = a + (b * c) / d     '  20 + (150/5)
+        Console.WriteLine("Value of a + (b * c) / d is  : {0}", e)
+        Console.ReadLine()
     End Sub
 
 End Module
+
+
+'When the above code Is compiled And executed, it produces the following result
+
+'Value of (a + b) * c / d Is : 90
+'Value of ((a + b) * c) / d Is  : 90
+'Value of (a + b) * (c / d) Is  : 90
+'Value of a + (b * c) / d Is  : 50
+
---
+++ T_022_VB_Decision_Making/T_022_VB_Decision_Making/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("T_022_VB_Decision_Making")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("T_022_VB_Decision_Making")>
+<Assembly: AssemblyCopyright("Copyright ©  2018")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("e91b1745-2dbf-4d14-8fdd-d0f588f461e4")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
