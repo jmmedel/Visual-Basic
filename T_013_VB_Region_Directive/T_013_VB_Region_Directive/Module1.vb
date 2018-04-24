677ac1576d3dba9f0e401a9c65b8ce6dc2ef828b
kagaya25 <32299866+kagaya25@users.noreply.github.com>
2018-04-24 11:06:08 +0900

    Done
    Done

--- T_013_VB_Region_Directive/T_013_VB_Region_Directive/Module1.vb
+++ T_013_VB_Region_Directive/T_013_VB_Region_Directive/Module1.vb
@@ -1,7 +1,26 @@
-﻿Module Module1
+﻿
+'The #Region Directive
+'This directive helps In collapsing And hiding sections Of code In Visual Basic files.
+
+'Syntax for this directive Is
+
+'#Region "identifier_string" 
+'#End Region
+
+
+#Region "StatsFunctions"
+' Insert code for the Statistical functions here.
+#End Region
+
+#Region "Module1"
+Module Module1
 
     Sub Main()
-
+        Console.WriteLine("Heelo ")
+        Console.ReadLine()
     End Sub
 
 End Module
+
+#End Region
+
