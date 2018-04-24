824f35aabec5c7ee3eb6ff2126d153606fc11076
kagaya25 <32299866+kagaya25@users.noreply.github.com>
2018-04-24 13:40:24 +0900

    Done
    Done

--- T_045_VB_Joining_String/T_045_VB_Joining_String/Module1.vb
+++ T_045_VB_Joining_String/T_045_VB_Joining_String/Module1.vb
@@ -1,7 +1,30 @@
-﻿Module Module1
+﻿
+'Joining Strings
+
+
+
+Module Module1
 
     Sub Main()
+        Dim strarray As String() = {"Down the way where the nights are gay",
+                                  "And the sun shines daily on the mountain top",
+                                   "I took a trip on a sailing ship",
+                                  "And when I reached Jamaica",
+                                  "I made a stop"}
+        Dim str As String = String.Join(vbCrLf, strarray)
+        Console.WriteLine(str)
+        Console.ReadLine()
 
     End Sub
 
 End Module
+
+'When the above code Is compiled And executed, it produces the following result:
+
+'Down the way where the nights are gay
+'And the sun shines daily on the mountain top
+'I took a trip On a sailing ship
+'And when I reached Jamaica
+'I made a Stop
+
+
---
+++ T_046_VB_Date_And_Time/T_046_VB_Date_And_Time/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("T_046_VB_Date_And_Time")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("T_046_VB_Date_And_Time")>
+<Assembly: AssemblyCopyright("Copyright ©  2018")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("edc5ea0c-91ab-4cc7-b954-4e2b45f270bb")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
