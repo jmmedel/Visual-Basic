2bcf1da5687d9a8a857e7828fd96443a002a4aa9
kagaya25 <32299866+kagaya25@users.noreply.github.com>
2018-04-24 10:32:09 +0900

    Done
    Done

--- T_006_VB_Accepting_Values_From_User/T_006_VB_Accepting_Values_From_User/Module1.vb
+++ T_006_VB_Accepting_Values_From_User/T_006_VB_Accepting_Values_From_User/Module1.vb
@@ -1,7 +1,26 @@
-﻿Module Module1
+﻿
+
+
+'Accepting Values from User
+'The Console Class In the System Namespace provides a Function ReadLine For accepting input from the user And store it into a variable. For example,
+
+'Dim message As String
+'message = Console.ReadLine
+'The following example demonstrates it
+
+
+Module Module1
 
     Sub Main()
-
+        Dim message As String
+        Console.Write("Enter message: ")
+        message = Console.ReadLine
+        Console.WriteLine()
+        Console.WriteLine("Your Message: {0}", message)
+        Console.ReadLine()
     End Sub
 
 End Module
+
+
+
