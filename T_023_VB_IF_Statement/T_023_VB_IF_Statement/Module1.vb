1658a5f3a50af962657b90e116f57bb1a08b6428
kagaya25 <32299866+kagaya25@users.noreply.github.com>
2018-04-24 11:41:18 +0900

    Done
    Done

--- T_023_VB_IF_Statement/T_023_VB_IF_Statement/Module1.vb
+++ T_023_VB_IF_Statement/T_023_VB_IF_Statement/Module1.vb
@@ -1,7 +1,37 @@
-﻿Module Module1
+﻿
+'VB.Net -If...Then Statement
+
+'It Is the simplest form Of control statement, frequently used in decision making And changing the control flow of the program execution. Syntax for if-then statement Is
+
+'If condition Then
+'[Statement(s)]
+'End If
+'Where, condition Is a Boolean Or relational condition And Statement(s) Is a simple Or compound statement. Example of an If-Then statement Is
+
+'If (a <= 20) Then
+'c = c + 1
+'End If
+'If the condition evaluates To True, Then the block Of code inside the If statement will be executed. If condition evaluates To False, Then the first Set Of code after the End Of the If statement (after the closing End If) will be executed.
+
+Module Module1
 
     Sub Main()
+        'local variable definition 
+        Dim a As Integer = 10
 
+        ' check the boolean condition using if statement 
+        If (a < 20) Then
+            ' if condition is true then print the following 
+            Console.WriteLine("a is less than 20")
+        End If
+        Console.WriteLine("value of a is : {0}", a)
+        Console.ReadLine()
     End Sub
 
 End Module
+
+
+'When the above code Is compiled And executed, it produces the following result
+
+'a Is less than 20
+'value of a Is : 10
---
+++ T_024_VB_IF_Else_Statement/T_024_IF_Else_Statement/Module1.vb
@@ -0,0 +1,41 @@
+﻿
+
+'VB.Net -If...Then...Else Statement
+
+'An If statement can be followed by an optional Else statement, which executes when the Boolean expression Is false.
+
+'Syntax:
+'The syntax Of an If...Then... Else statement In VB.Net Is As follows
+
+'If (boolean_expression) Then
+''statement(s) will execute if the Boolean expression is true 
+'Else
+''statement(s) will execute if the Boolean expression is false 
+'End If
+'If the Boolean expression evaluates To True, Then the If block Of code will be executed, otherwise Else block Of code will be executed.
+
+Module Module1
+
+    Sub Main()
+        'local variable definition '
+        Dim a As Integer = 100
+
+        ' check the boolean condition using if statement 
+        If (a < 20) Then
+            ' if condition is true then print the following 
+            Console.WriteLine("a is less than 20")
+        Else
+            ' if condition is false then print the following 
+            Console.WriteLine("a is not less than 20")
+        End If
+        Console.WriteLine("value of a is : {0}", a)
+        Console.ReadLine()
+    End Sub
+
+
+End Module
+
+'When the above code Is compiled And executed, it produces the following result
+
+'a Is Not less than 20
+'value of a Is : 100
