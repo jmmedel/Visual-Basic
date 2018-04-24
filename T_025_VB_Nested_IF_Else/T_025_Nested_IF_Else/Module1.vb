14a99bbcd25c99d7ac8f32007007715235b7650b
kagaya25 <32299866+kagaya25@users.noreply.github.com>
2018-04-24 11:45:55 +0900

    Done
    Done

--- T_025_VB_Nested_IF_Else/T_025_Nested_IF_Else/Module1.vb
+++ T_025_VB_Nested_IF_Else/T_025_Nested_IF_Else/Module1.vb
@@ -1,7 +1,57 @@
-﻿Module Module1
+﻿
+'The If...Else If...Else Statement
+'An If statement can be followed by an optional Else if...Else statement, which Is very useful to test various conditions using single If...Else If statement.
+
+'When using If... Else If... Else statements, there are few points to keep in mind.
+
+'An If can have zero Or one Else's and it must come after an Else If's.
+
+'An If can have zero to many Else If's and they must come before the Else.
+
+'Once an Else If succeeds, none Of the remaining Else If's or Else's will be tested.
+
+'Syntax:
+'The syntax Of an If...Else If...Else statement In VB.Net Is As follows
+
+'If (boolean_expression 1)Then
+'' Executes when the boolean expression 1 is true 
+'ElseIf (boolean_expression 2)Then
+'' Executes when the boolean expression 2 is true 
+'ElseIf (boolean_expression 3)Then
+'' Executes when the boolean expression 3 is true 
+'Else
+'' executes when the none of the above condition is true 
+'End If
+
+
+
+Module Module1
 
     Sub Main()
-
+        'local variable definition '
+        Dim a As Integer = 100
+        ' check the boolean condition '
+        If (a = 10) Then
+            ' if condition is true then print the following '
+            Console.WriteLine("Value of a is 10") '
+        ElseIf (a = 20) Then
+            'if else if condition is true '
+            Console.WriteLine("Value of a is 20") '
+        ElseIf (a = 30) Then
+            'if else if condition is true  
+            Console.WriteLine("Value of a is 30")
+        Else
+            'if none of the conditions is true 
+            Console.WriteLine("None of the values is matching")
+        End If
+        Console.WriteLine("Exact value of a is: {0}", a)
+        Console.ReadLine()
     End Sub
 
 End Module
+
+
+'When the above code Is compiled And executed, it produces the following result
+
+'None of the values Is matching
+'Exact value Of a Is: 100
---
+++ T_026_VB_Nested_IF/T_026_VB_Nested_IF/Module1.vb
@@ -0,0 +1,45 @@
+﻿
+
+'VB.Net -Nested If Statements
+'It Is always legal In VB.Net To nest If-Then-Else statements, which means you can use one If Or ElseIf statement inside another If ElseIf statement(s).
+
+'Syntax:
+'The syntax For a nested If statement Is As follows
+
+'If (boolean_expression 1)Then
+''Executes when the boolean expression 1 is true 
+'If (boolean_expression 2)Then
+''Executes when the boolean expression 2 is true 
+'End If
+'End If
+'You can nest ElseIf...Else In the similar way As you have nested If statement.
+
+
+Module Module1
+
+    Sub Main()
+        'local variable definition
+        Dim a As Integer = 100
+        Dim b As Integer = 200
+        ' check the boolean condition 
+        If (a = 100) Then
+            ' if condition is true then check the following 
+            If (b = 200) Then
+                ' if condition is true then print the following 
+                Console.WriteLine("Value of a is 100 and b is 200")
+            End If
+        End If
+        Console.WriteLine("Exact value of a is : {0}", a)
+        Console.WriteLine("Exact value of b is : {0}", b)
+        Console.ReadLine()
+    End Sub
+
+
+End Module
+
+
+'When the above code Is compiled And executed, it produces the following result
+
+'Value of a Is 100 And b Is 200
+'Exact value Of a Is : 100
+'Exact value Of b Is : 200
