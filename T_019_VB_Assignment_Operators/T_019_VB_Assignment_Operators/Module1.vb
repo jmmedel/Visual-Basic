d52bfff5f967898f6c46e6801789266283113bde
kagaya25 <32299866+kagaya25@users.noreply.github.com>
2018-04-24 11:30:27 +0900

    Done
    Done

--- T_019_VB_Assignment_Operators/T_019_VB_Assignment_Operators/Module1.vb
+++ T_019_VB_Assignment_Operators/T_019_VB_Assignment_Operators/Module1.vb
@@ -1,7 +1,70 @@
-﻿Module Module1
+﻿
+'VB.Net -Assignment Operators
+'There are following assignment operators supported by VB.Net
+
+'Operator    Description	Example
+'=	Simple assignment operator, Assigns values from right side operands to left side operand	C = A + B will assign value of A + B into C
+'+=	Add And assignment operator, It adds right operand to the left operand And assigns the result to left operand	C += A Is equivalent to C = C + A
+'-=	Subtract And assignment operator, It subtracts right operand from the left operand And assigns the result to left operand	C -= A Is equivalent to C = C - A
+'*=	Multiply And assignment operator, It multiplies right operand with the left operand And assigns the result to left operand	C *= A Is equivalent to C = C * A
+'/=	Divide And assignment operator, It divides left operand with the right operand And assigns the result to left operand (floating point division)	C /= A Is equivalent to C = C / A
+'\=	Divide And assignment operator, It divides left operand with the right operand And assigns the result to left operand (Integer division)	C \= A Is equivalent to C = C \A
+'^=	Exponentiation And assignment operator. It raises the left operand to the power of the right operand And assigns the result to left operand	C^=A Is equivalent to C = C ^ A
+'<<=	Left shift And assignment operator	C <<= 2 Is same as C = C << 2
+'>>=	Right shift And assignment operator	C >>= 2 Is same as C = C >> 2
+'&=	Concatenates a String expression to a String variable Or property And assigns the result to the variable Or property.
+
+Module Module1
 
     Sub Main()
-
+        Dim a As Integer = 21
+        Dim pow As Integer = 2
+        Dim str1 As String = "Hello! "
+        Dim str2 As String = "VB Programmers"
+        Dim c As Integer
+        c = a
+        Console.WriteLine("Line 1 - =  Operator Example, _
+	  Value of c = {0}", c)
+        c += a
+        Console.WriteLine("Line 2 - +=  Operator Example, _
+	  Value of c = {0}", c)
+        c -= a
+        Console.WriteLine("Line 3 - -=  Operator Example, _
+	  Value of c = {0}", c)
+        c *= a
+        Console.WriteLine("Line 4 - *=  Operator Example, _
+	  Value of c = {0}", c)
+        c /= a
+        Console.WriteLine("Line 5 - /=  Operator Example, _
+	  Value of c = {0}", c)
+        c = 20
+        c ^= pow
+        Console.WriteLine("Line 6 - ^=  Operator Example, _
+	  Value of c = {0}", c)
+        c <<= 2
+        Console.WriteLine("Line 7 - <<=  Operator Example,_
+	  Value of c = {0}", c)
+        c >>= 2
+        Console.WriteLine("Line 8 - >>=  Operator Example,_
+	  Value of c = {0}", c)
+        str1 &= str2
+        Console.WriteLine("Line 9 - &=  Operator Example,_
+	  Value of str1 = {0}", str1)
+        Console.ReadLine()
     End Sub
 
+
 End Module
+
+
+'When the above code is compiled and executed, it produces the following result:
+
+'Line 1 - =  Operator Example, Value of c = 21
+'Line 2 - += Operator Example, Value of c = 42
+'Line 3 - -= Operator Example, Value of c = 21
+'Line 4 - *= Operator Example, Value of c = 441
+'Line 5 - /= Operator Example, Value of c = 21
+'Line 6 - ^= Operator Example, Value of c = 400
+'Line 7 - <<= Operator Example, Value of c = 1600
+'Line 8 - >>= Operator Example, Value of c = 400
+'Line 9 - &= Operator Example, Value of str1 = Hello! VB Programmers
---
+++ T_020_VB_Miscellaneous_Operator/T_020_VB_Miscellaneous_Operator/Module1.vb
@@ -0,0 +1,51 @@
+﻿
+
+'VB.Net -Miscellaneous Operators
+'There are few other important operators supported by VB.Net.
+
+'Operator    Description	Example
+'AddressOf	Returns the address of a procedure.	
+'AddHandler Button1.Click,
+'AddressOf Button1_Click
+'    Await   It Is applied to an operand in an asynchronous method Or lambda expression to suspend execution of the method until the awaited task completes.	
+
+'Dim result As Res
+'= Await AsyncMethodThatReturnsResult()
+'Await AsyncMethod()
+'GetType It returns a Type object for the specified type. The Type object provides information about the type such as its properties, methods, And events.	
+'MsgBox(GetType(Integer).ToString())
+'Function Expression()	It declares the parameters And code that define a Function lambda expression.	
+'Dim add5 = Function(num As
+' Integer) num + 5
+''prints 10
+'Console.WriteLine(add5(5))
+'               If It Then uses Short-circuit evaluation To conditionally Return one Of two values. The If Operator can be called With three arguments Or With two arguments.	
+'Dim num = 5
+'                   Console.WriteLine(If(num >= 0,
+'                   "Positive", "Negative"))
+
+
+
+Module Module1
+
+    Sub Main()
+        Dim a As Integer = 21
+        Console.WriteLine(GetType(Integer).ToString())
+        Console.WriteLine(GetType(Double).ToString())
+        Console.WriteLine(GetType(String).ToString())
+        Dim multiplywith5 = Function(num As Integer) num * 5
+        Console.WriteLine(multiplywith5(5))
+        Console.WriteLine(If(a >= 0, "Positive", "Negative"))
+        Console.ReadLine()
+    End Sub
+
+
+End Module
+
+'When the above code Is compiled And executed, it produces the following result
+
+'System.Int32
+'System.Double
+'System.String
+'25
+'Positive
