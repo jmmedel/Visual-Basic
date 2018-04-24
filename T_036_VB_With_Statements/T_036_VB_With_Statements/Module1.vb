727af4a2dc6f842a4035441f4b680a400406d445
kagaya25 <32299866+kagaya25@users.noreply.github.com>
2018-04-24 13:16:55 +0900

    Done
    Done

--- T_036_VB_With_Statements/T_036_VB_With_Statements/Module1.vb
+++ T_036_VB_With_Statements/T_036_VB_With_Statements/Module1.vb
@@ -1,7 +1,34 @@
-﻿Module Module1
+﻿
+'VB.Net - With... End With Statement
+'It Is Not exactly a looping construct. It executes a series of statements that repeatedly refers to a single object Or structure.
 
+'The syntax For this Loop construct Is
+
+'With Object
+'    [ statements ]
+'End With
+
+
+Module Module1
+
+    Public Class Book
+        Public Property Name As String
+        Public Property Author As String
+        Public Property Subject As String
+    End Class
     Sub Main()
-
+        Dim aBook As New Book
+        With aBook
+            .Name = "VB.Net Programming"
+            .Author = "Kagaya john "
+            .Subject = "Information Technology"
+        End With
+        With aBook
+            Console.WriteLine(.Name)
+            Console.WriteLine(.Author)
+            Console.WriteLine(.Subject)
+        End With
+        Console.ReadLine()
     End Sub
 
 End Module
