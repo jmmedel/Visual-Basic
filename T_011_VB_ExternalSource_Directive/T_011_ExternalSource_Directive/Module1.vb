959c4f7a0e629b15ea040154f49917ba81596144
kagaya25 <32299866+kagaya25@users.noreply.github.com>
2018-04-24 11:03:04 +0900

    Done
    Done

--- T_011_VB_ExternalSource_Directive/T_011_ExternalSource_Directive/Module1.vb
+++ T_011_VB_ExternalSource_Directive/T_011_ExternalSource_Directive/Module1.vb
@@ -1,11 +1,47 @@
 ﻿
 
+'The #ExternalSource Directive
+'This directive Is used For indicating a mapping between specific lines Of source code And text external To the source. It Is used only by the compiler And the debugger has no effect On code compilation.
+
+'This directive allows including external code from an external code file into a source code file.
+
+'Syntax for this directive Is:
+
+'#ExternalSource( StringLiteral , IntLiteral )
+'    [ LogicalLine ]
+'#End ExternalSource
+'The parameters Of #ExternalSource directive are the path Of external file, line number Of the first line, And the line where the Error occurred.
+
+'Example
+
+'The following code demonstrates a hypothetical use Of the directive
 
 
 Module Module1
 
+    Public Class ExternalSourceTester
+
+        Sub TestExternalSource()
+
+#ExternalSource ("c:\vbprogs\directives.vb", 5)
+            Console.WriteLine("This is External Code. ")
+#End ExternalSource
+
+        End Sub
+    End Class
+
     Sub Main()
+        Dim t As New ExternalSourceTester()
+        t.TestExternalSource()
+        Console.WriteLine("In Main.")
+        Console.ReadKey()
 
     End Sub
 
 End Module
+
+
+'When the above code Is compiled And executed, it produces the following result
+
+'This Is External Code.
+'In Main.
---
+++ T_012_VB_IF_Else_Directives/T_012_IF_Else_Directives/Module1.vb
@@ -0,0 +1,47 @@
+﻿
+
+''The #If...Then...#Else Directives
+''This directive conditionally compiles selected blocks Of Visual Basic code.
+
+''Syntax for this directive Is
+
+''#If expression Then
+''   statements
+''[ #ElseIf expression Then
+''   [ statements ]
+''...
+''#ElseIf expression Then
+''   [ statements ] ]
+''[ #Else
+''   [ statements ] ]
+''#End If
+
+'For example,
+
+'#Const TargetOS = "Linux"
+'#If TargetOS = "Windows 7" Then
+'   ' Windows 7 specific code
+'#ElseIf TargetOS = "WinXP" Then
+'   ' Windows XP specific code
+'#Else
+'   ' Code for other OS
+'#End If
+
+
+Module Module1
+
+#Const classCode = 8
+
+    Sub Main()
+#If classCode = 7 Then
+        Console.WriteLine("Exam Questions for Class VII")
+#ElseIf classCode = 8 Then
+        Console.WriteLine("Exam Questions for Class VIII")
+#Else
+        Console.WriteLine("Exam Questions for Higher Classes")
+#End If
+        Console.ReadKey()
+
+    End Sub
+
+End Module
