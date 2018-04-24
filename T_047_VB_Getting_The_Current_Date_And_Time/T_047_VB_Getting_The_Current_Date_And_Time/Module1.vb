49a3a86845cc55182b0721fc28b3204a0821d463
kagaya25 <32299866+kagaya25@users.noreply.github.com>
2018-04-24 13:44:42 +0900

    Done
    Done

--- T_047_VB_Getting_The_Current_Date_And_Time/T_047_VB_Getting_The_Current_Date_And_Time/Module1.vb
+++ T_047_VB_Getting_The_Current_Date_And_Time/T_047_VB_Getting_The_Current_Date_And_Time/Module1.vb
@@ -1,6 +1,21 @@
-﻿Module Module1
+﻿
+
+
+
+'Getting the Current Date And Time:
+'The following programs demonstrate how To Get the current Date And time In VB.Net
+
+Module Module1
 
     Sub Main()
+
+        Console.Write("Current Time: ")
+        Console.WriteLine(Now.ToLongTimeString)
+        Console.WriteLine("Current Date: ")
+        Dim dt As Date = Today
+        Console.WriteLine("Today is: {0}", dt)
+
+        Console.ReadKey()
 
     End Sub
 
