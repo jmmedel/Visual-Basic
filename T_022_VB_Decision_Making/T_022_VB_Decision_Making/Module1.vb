692a3e7a59a7dd889c49fd5aed65c36d352d8d53
kagaya25 <32299866+kagaya25@users.noreply.github.com>
2018-04-24 11:35:20 +0900

    Done
    Done

--- T_022_VB_Decision_Making/T_022_VB_Decision_Making/Module1.vb
+++ T_022_VB_Decision_Making/T_022_VB_Decision_Making/Module1.vb
@@ -1,4 +1,32 @@
-﻿Module Module1
+﻿
+
+'VB.Net -Decision Making
+'Decision making structures require that the programmer specify one Or more conditions To be evaluated Or tested by the program, along With a statement Or statements To be executed If the condition Is determined To be True, And optionally, other statements To be executed If the condition Is determined To be False.
+
+'Following Is the general form of a typical decision making structure found in most of the programming languages:
+
+'Decision making statements In VB.Net
+'VB.Net provides the following types of decision making statements. Click the following links to check their details.
+
+'Statement	Description
+'If ... Then statement
+
+'An If...Then statement consists of a boolean expression followed by one Or more statements.
+'If ...Then...Else statement
+
+'An If...Then statement can be followed by an optional Else statement, which executes when the boolean expression Is false.
+'nested If statements
+
+'You can use one If Or Else If statement inside another If Or Else If statement(s).
+'Select Case statement
+
+'A Select Case statement allows a variable to be tested for equality against a list of values.
+'nested Select Case statements
+
+'You can use one Select Case statement inside another Select Case statement(s).
+
+
+Module Module1
 
     Sub Main()
 
