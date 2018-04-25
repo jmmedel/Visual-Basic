9e72cfee7057cf214faea796d38a9b2ed3d3a5ab
kagaya25 <32299866+kagaya25@users.noreply.github.com>
2018-04-26 02:25:46 +0900

    Done
    Done

---
+++ T_075_VB_Shared_Members_Of_A_Vb/T_075_VB_Shared_Members_Of_A_Vb/Module1.vb
@@ -0,0 +1,36 @@
+﻿
+'Shared Members Of a VB.Net Class
+'We can define Class members As Static Using the Shared keyword. When we Declare a member Of a Class As Shared, it means no matter how many objects Of the Class are created, there Is only one copy Of the member.
+
+'The keyword Shared implies that only one instance Of the member exists For a Class. Shared variables are used For defining constants because their values can be retrieved by invoking the Class without creating an instance Of it.
+
+'Shared variables can be initialized outside the member Function Or Class definition. You can also initialize Shared variables inside the Class definition.
+
+'You can also Declare a member Function As Shared. Such functions can access only Shared variables. The Shared functions exist even before the Object Is created.
+
+'The following example demonstrates the use Of Shared members
+
+
+Module Module1
+
+    Class StaticVar
+        Public Shared num As Integer
+        Public Sub count()
+            num = num + 1
+        End Sub
+        Public Shared Function getNum() As Integer
+            Return num
+        End Function
+
+    End Class
+
+    Sub Main()
+        Dim s As StaticVar = New StaticVar()
+        s.count()
+        s.count()
+        s.count()
+        Console.WriteLine("Value of variable num: {0}", StaticVar.getNum())
+        Console.ReadKey()
+    End Sub
+
+End Module
---
+++ T_075_VB_Shared_Members_Of_A_Vb/T_075_VB_Shared_Members_Of_A_Vb/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("T_075_VB_Shared_Members_Of_A_Vb")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("T_075_VB_Shared_Members_Of_A_Vb")>
+<Assembly: AssemblyCopyright("Copyright ©  2018")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("c4ba3b0a-7f48-4a47-8e3a-34cf1f1a9fac")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
