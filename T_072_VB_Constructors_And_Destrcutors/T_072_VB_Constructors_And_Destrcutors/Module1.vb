b5eef02101e927b95e952942f8f4b0ada05fe254
kagaya25 <32299866+kagaya25@users.noreply.github.com>
2018-04-26 02:17:47 +0900

    Done
    Done

---
+++ T_072_VB_Constructors_And_Destrcutors/T_072_VB_Constructors_And_Destrcutors/Module1.vb
@@ -0,0 +1,38 @@
+﻿
+'Constructors And Destructors
+'A Class constructor Is a special member Sub Of a Class that Is executed whenever we create New objects Of that Class. A constructor has the name New And it does Not have any Return type.
+
+'Following program explains the concept Of constructor
+
+
+Module Module1
+    Class Line
+        Private length As Double    ' Length of a line
+        Public Sub New()   'constructor
+            Console.WriteLine("Object is being created")
+        End Sub
+        Public Sub setLength(ByVal len As Double)
+            length = len
+        End Sub
+
+        Public Function getLength() As Double
+            Return length
+        End Function
+
+    End Class
+
+    Sub Main()
+        Dim line As Line = New Line()
+        'set line length
+        line.setLength(6.0)
+        Console.WriteLine("Length of line : {0}", line.getLength())
+        Console.ReadKey()
+    End Sub
+
+End Module
+
+
+'When the above code Is compiled And executed, it produces the following result
+
+'Object Is being created
+'Length of line : 6
---
+++ T_073_VB_Constrcutor_parameterized/T_073_VB_Constrcutor_parameterized/Module1.vb
@@ -0,0 +1,34 @@
+﻿
+'A Default constructor does Not have any parameter, but If you need, a constructor can have parameters. Such constructors are called parameterized constructors. This technique helps you To assign initial value To an Object at the time Of its 
+'    as shown in the following example
+
+
+Module Module1
+
+    Class Line
+        Private length As Double    ' Length of a line
+        Public Sub New(ByVal len As Double)   'parameterised constructor
+            Console.WriteLine("Object is being created, length = {0}", len)
+            length = len
+        End Sub
+        Public Sub setLength(ByVal len As Double)
+            length = len
+        End Sub
+
+        Public Function getLength() As Double
+            Return length
+        End Function
+
+
+    End Class
+
+    Sub Main()
+        Dim line As Line = New Line(10.0)
+        Console.WriteLine("Length of line set by constructor : {0}", line.getLength())
+        'set line length
+        line.setLength(6.0)
+        Console.WriteLine("Length of line set by setLength : {0}", line.getLength())
+        Console.ReadKey()
+    End Sub
+
+End Module
