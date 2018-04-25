5f0f285997af2b1962c1cf106424141079ff5d76
kagaya25 <32299866+kagaya25@users.noreply.github.com>
2018-04-26 02:09:56 +0900

    Done
    doone

--- kadai08/kadai08/Form1.vb
+++ kadai08/kadai08/Form1.vb
@@ -1,3 +1,8 @@
 ﻿Public Class Form1
-
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim year As Integer
+        year = TextBox1.Text
+        year = year / 100
+        Label2.Text = year & "世紀"
+    End Sub
 End Class
