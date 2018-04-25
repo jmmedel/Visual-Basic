84581c6ab42d8f476d824b2c7bf7097279a013b5
kagaya25 <32299866+kagaya25@users.noreply.github.com>
2018-04-26 02:22:50 +0900

    Done
    Done

--- kadai09/kadai09/Form1.vb
+++ kadai09/kadai09/Form1.vb
@@ -1,3 +1,23 @@
 ﻿Public Class Form1
 
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim b As Integer = TextBox1.Text
+
+
+        Select Case b
+            Case 3 To 5
+                Label2.Text = "春です"
+            Case 6 To 8
+                Label2.Text = "夏です"
+            Case 9 To 11
+                Label2.Text = "秋です"
+            Case 12, 1, 2
+                Label2.Text = "冬です"
+            Case Else
+                Label2.Text = "エラです"
+
+        End Select
+
+    End Sub
+
 End Class
---
+++ kadai10/kadai10/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("kadai10")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("kadai10")>
+<Assembly: AssemblyCopyright("Copyright ©  2018")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("17d8ecde-389b-4eb2-b870-597bcd76778a")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
