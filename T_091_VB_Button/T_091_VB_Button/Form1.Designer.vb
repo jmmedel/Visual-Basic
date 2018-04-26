26a7a73532c05a6972bb83500963e8f560c26188
kagaya25 <32299866+kagaya25@users.noreply.github.com>
2018-04-26 21:42:53 +0900

    Done
    Done

--- T_091_VB_Button/T_091_VB_Button/Form1.Designer.vb
+++ T_091_VB_Button/T_091_VB_Button/Form1.Designer.vb
@@ -22,9 +22,80 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.btnMoto = New System.Windows.Forms.Button()
+        Me.btnLogo = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.btnImage = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'btnMoto
+        '
+        Me.btnMoto.Location = New System.Drawing.Point(12, 198)
+        Me.btnMoto.Name = "btnMoto"
+        Me.btnMoto.Size = New System.Drawing.Size(75, 23)
+        Me.btnMoto.TabIndex = 0
+        Me.btnMoto.Text = "Moto"
+        Me.btnMoto.UseVisualStyleBackColor = True
+        '
+        'btnLogo
+        '
+        Me.btnLogo.Location = New System.Drawing.Point(105, 198)
+        Me.btnLogo.Name = "btnLogo"
+        Me.btnLogo.Size = New System.Drawing.Size(75, 23)
+        Me.btnLogo.TabIndex = 1
+        Me.btnLogo.Text = "Show Logo"
+        Me.btnLogo.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(197, 198)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(75, 23)
+        Me.btnExit.TabIndex = 2
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(40, 43)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(38, 12)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "Label1"
+        '
+        'btnImage
+        '
+        Me.btnImage.Image = CType(resources.GetObject("btnImage.Image"), System.Drawing.Image)
+        Me.btnImage.Location = New System.Drawing.Point(128, 12)
+        Me.btnImage.Name = "btnImage"
+        Me.btnImage.Size = New System.Drawing.Size(264, 161)
+        Me.btnImage.TabIndex = 4
+        Me.btnImage.Text = "Button1"
+        Me.btnImage.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(474, 261)
+        Me.Controls.Add(Me.btnImage)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnLogo)
+        Me.Controls.Add(Me.btnMoto)
+        Me.Name = "Form1"
         Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
 
+    Friend WithEvents btnMoto As Button
+    Friend WithEvents btnLogo As Button
+    Friend WithEvents btnExit As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents btnImage As Button
 End Class
--- T_091_VB_Button/T_091_VB_Button/Form1.vb
+++ T_091_VB_Button/T_091_VB_Button/Form1.vb
@@ -1,3 +1,97 @@
-﻿Public Class Form1
+﻿
+'VB.Net -Button Control
+'The Button control represents a standard Windows button. It Is generally used To generate a Click Event by providing a handler For the Click Event.
 
+'Let's create a label by dragging a Button control from the Toolbox ad dropping it on the form.
+'Properties of the Button Control
+'The following are some Of the commonly used properties Of the Button control
+
+'S.N  Property	Description
+'1   AutoSizeMode	Gets Or sets the mode by which the Button automatically resizes itself.
+'2   BackColor	Gets Or sets the background color of the control.
+'3   BackgroundImage	Gets Or sets the background image displayed in the control.
+'4   DialogResult	Gets Or sets a value that Is returned to the parent form when the button Is clicked. This Is used while creating dialog boxes.
+'5   ForeColor	Gets Or sets the foreground color of the control.
+'6   Image	Gets Or sets the image that Is displayed on a button control.
+'7   Location	Gets Or sets the coordinates of the upper-left corner of the control relative to the upper-left corner of its container.
+'8   TabIndex	Gets Or sets the tab order of the control within its container.
+'9   Text	Gets Or sets the text associated with this control.
+'Methods of the Button Control
+'The following are some Of the commonly used methods Of the Button control
+
+'S.N  Method Name & Description
+'1   
+'GetPreferredSize
+
+'Retrieves the size Of a rectangular area into which a control can be fitted.
+
+'2   
+'NotifyDefault
+
+'Notifies the Button whether it Is the Default button so that it can adjust its appearance accordingly.
+
+'3   
+'Select Case
+
+'Activates the control.
+
+'4   
+'ToString
+
+'Returns a String containing the name Of the Component, If any. This method should Not be overridden.
+
+'Events of the Button Control
+'The following are some Of the commonly used events Of the Button control
+
+'S.N  Event	Description
+'1   Click	Occurs when the control Is clicked.
+'2   DoubleClick	Occurs when the user double-clicks the Button control.
+'3   GotFocus	Occurs when the control receives focus.
+'4   TabIndexChanged	Occurs when the TabIndex property value changes.
+'5   TextChanged	Occurs when the Text property value changes.
+'6   Validated	Occurs when the control Is finished validating.
+'Consult Microsoft documentation For detailed list Of properties, methods And events Of the Button control.
+
+'Example
+'In the following example, we create three buttons. In this example, let us
+
+'Set captions For the buttons
+
+'Set some image For the button
+
+'Handle the click events Of Each buttons
+
+'Take following steps
+
+'Drag And drop a Label control on the form.
+
+'Set the Text Property To provide the caption "Tutorials Point".
+
+'Drag And drop three buttons on the form.
+
+'Using the properties window, change the Name properties Of the buttons To btnMoto, btnLogo And btnExit respectively.
+
+'Using the properties window, change the Text properties Of the buttons To Show Moto, Show Logo And Exit respectively.
+
+'Drag And Drop another button, using the properties window, set its Image property And name it btnImage.
+
+'At this stage, the form looks Like
+
+Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ' Set the caption bar text of the form.  
+        Me.Text = "tutorialspont.com"
+        btnImage.Visible = False
+    End Sub
+    Private Sub btnMoto_Click(sender As Object, e As EventArgs) Handles btnMoto.Click
+        btnImage.Visible = False
+        Label1.Text = "Simple Easy Learning"
+    End Sub
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Application.Exit()
+    End Sub
+    Private Sub btnLogo_Click(sender As Object, e As EventArgs) Handles btnLogo.Click
+        Label1.Visible = False
+        btnImage.Visible = True
+    End Sub
 End Class
