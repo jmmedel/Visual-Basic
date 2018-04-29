faa3322724fb8a2b32caf2a653593f9bde3edce7
kagaya25 <32299866+kagaya25@users.noreply.github.com>
2018-04-30 02:37:38 +0900

    Done
    Done

---
+++ project004_Add_Watermark/project004_Add_Watermark/Form1.vb
@@ -0,0 +1,8 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim NF As New Font("Impact", 36, FontStyle.Italic)
+        Dim NB As New SolidBrush(Color.FromArgb(64, 192, 255, 255))
+        NB.Color = Color.FromArgb(128, 0, 0, 0)
+        PictureBox1.CreateGraphics.DrawString(TextBox1.Text, NF, NB, 15, 135)
+    End Sub
+End Class
---
+++ project004_Add_Watermark/project004_Add_Watermark/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("project004_Add_Watermark")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("project004_Add_Watermark")>
+<Assembly: AssemblyCopyright("Copyright ©  2018")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("374da137-5042-4693-9e07-f8eaf96214b1")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
