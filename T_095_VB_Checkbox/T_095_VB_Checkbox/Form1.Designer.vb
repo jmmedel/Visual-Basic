d9b56d80f037755a9c137f5ea399b8e7f36d868b
kagaya25 <32299866+kagaya25@users.noreply.github.com>
2018-05-02 02:24:49 +0900

    Done
    Done

--- T_095_VB_Checkbox/T_095_VB_Checkbox/Form1.Designer.vb
+++ T_095_VB_Checkbox/T_095_VB_Checkbox/Form1.Designer.vb
@@ -22,9 +22,119 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.CheckBox1 = New System.Windows.Forms.CheckBox()
+        Me.CheckBox2 = New System.Windows.Forms.CheckBox()
+        Me.CheckBox3 = New System.Windows.Forms.CheckBox()
+        Me.CheckBox4 = New System.Windows.Forms.CheckBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.GroupBox1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.CheckBox4)
+        Me.GroupBox1.Controls.Add(Me.CheckBox3)
+        Me.GroupBox1.Controls.Add(Me.CheckBox2)
+        Me.GroupBox1.Controls.Add(Me.CheckBox1)
+        Me.GroupBox1.Location = New System.Drawing.Point(38, 41)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(200, 100)
+        Me.GroupBox1.TabIndex = 0
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Came to know about us from'"
+        '
+        'CheckBox1
+        '
+        Me.CheckBox1.AutoSize = True
+        Me.CheckBox1.Location = New System.Drawing.Point(17, 33)
+        Me.CheckBox1.Name = "CheckBox1"
+        Me.CheckBox1.Size = New System.Drawing.Size(62, 16)
+        Me.CheckBox1.TabIndex = 0
+        Me.CheckBox1.Text = "Friends"
+        Me.CheckBox1.UseVisualStyleBackColor = True
+        '
+        'CheckBox2
+        '
+        Me.CheckBox2.AutoSize = True
+        Me.CheckBox2.Location = New System.Drawing.Point(17, 55)
+        Me.CheckBox2.Name = "CheckBox2"
+        Me.CheckBox2.Size = New System.Drawing.Size(64, 16)
+        Me.CheckBox2.TabIndex = 1
+        Me.CheckBox2.Text = "Website"
+        Me.CheckBox2.UseVisualStyleBackColor = True
+        '
+        'CheckBox3
+        '
+        Me.CheckBox3.AutoSize = True
+        Me.CheckBox3.Location = New System.Drawing.Point(112, 33)
+        Me.CheckBox3.Name = "CheckBox3"
+        Me.CheckBox3.Size = New System.Drawing.Size(91, 16)
+        Me.CheckBox3.TabIndex = 2
+        Me.CheckBox3.Text = "News Papers"
+        Me.CheckBox3.UseVisualStyleBackColor = True
+        '
+        'CheckBox4
+        '
+        Me.CheckBox4.AutoSize = True
+        Me.CheckBox4.Location = New System.Drawing.Point(112, 55)
+        Me.CheckBox4.Name = "CheckBox4"
+        Me.CheckBox4.Size = New System.Drawing.Size(58, 16)
+        Me.CheckBox4.TabIndex = 3
+        Me.CheckBox4.Text = "Others"
+        Me.CheckBox4.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(38, 164)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(89, 12)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "If others specify"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(40, 190)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(192, 19)
+        Me.TextBox1.TabIndex = 2
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(90, 225)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 35)
+        Me.Button1.TabIndex = 3
+        Me.Button1.Text = "Submit"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(284, 283)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Name = "Form1"
         Me.Text = "Form1"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
 
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents CheckBox4 As CheckBox
+    Friend WithEvents CheckBox3 As CheckBox
+    Friend WithEvents CheckBox2 As CheckBox
+    Friend WithEvents CheckBox1 As CheckBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Button1 As Button
 End Class
--- T_095_VB_Checkbox/T_095_VB_Checkbox/Form1.vb
+++ T_095_VB_Checkbox/T_095_VB_Checkbox/Form1.vb
@@ -1,3 +1,91 @@
-﻿Public Class Form1
+﻿
+'VB.Net -CheckBox Control
+'The CheckBox control allows the user To Set True/False Or yes/no type options. The user can Select Or deselect it. When a check box Is selected it has the value True, And When it Is cleared, it holds the value False.
 
+'Let's create two check boxes by dragging CheckBox controls from the Toolbox and dropping on the form.
+'The CheckBox control has three states, checked, unchecked And indeterminate. In the indeterminate state, the check box Is grayed out. To enable the indeterminate state, the ThreeState Property Of the check box Is Set To be True.
+
+'Properties of the CheckBox Control
+'The following are some Of the commonly used properties Of the CheckBox control
+
+'S.N  Property	Description
+'1   Appearance	Gets Or sets a value determining the appearance of the check box.
+'2   AutoCheck	Gets Or sets a value indicating whether the Checked Or CheckedState value And the appearance of the control automatically change when the check box Is selected.
+'3   CheckAlign	Gets Or sets the horizontal And vertical alignment of the check mark on the check box.
+'4   Checked	Gets Or sets a value indicating whether the check box Is selected.
+'5   CheckState	Gets Or sets the state of a check box.
+'6   Text	Gets Or sets the caption of a check box.
+'7   ThreeState	Gets Or sets a value indicating whether Or Not a check box should allow three check states rather than two.
+'Methods of the CheckBox Control
+'The following are some Of the commonly used methods Of the CheckBox control
+
+'S.N  Method Name & Description
+'1   
+'OnCheckedChanged
+
+'Raises the CheckedChanged Event.
+
+'2   
+'OnCheckStateChanged
+
+'Raises the CheckStateChanged Event.
+
+'3   
+'OnClick
+
+'Raises the OnClick Event.
+
+'Events of the CheckBox Control
+'The following are some Of the commonly used events Of the CheckBox control
+
+'S.N  Event	Description
+'1   AppearanceChanged	Occurs when the value of the Appearance property of the check box Is changed.
+'2   CheckedChanged	Occurs when the value of the Checked property of the CheckBox control Is changed.
+'3   CheckStateChanged	Occurs when the value of the CheckState property of the CheckBox control Is changed.
+'Consult Microsoft documentation For detailed list Of properties, methods And events Of the CheckBox control.
+
+'Example
+'In this example, let us add four check boxes in a group box. The check boxes will allow the users to choose the source from which they came to know about the organization. If the user chooses the check box with text "others", then the user Is asked to specify And a text box Is provided to give input. When the user clicks the Submit button, he/she gets an appropriate message.
+
+'The form In design view
+
+Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) _
+       Handles MyBase.Load
+        ' Set the caption bar text of the form.  
+        Me.Text = "tutorialspoint.com"
+        Label1.Visible = False
+        TextBox1.Visible = False
+        TextBox1.Multiline = True
+    End Sub
+    Private Sub Button1_Click(sender As Object, e As EventArgs) _
+      Handles Button1.Click
+        Dim str As String
+        str = " "
+        If CheckBox1.Checked = True Then
+            str &= CheckBox1.Text
+            str &= " "
+        End If
+        If CheckBox2.Checked = True Then
+            str &= CheckBox2.Text
+            str &= " "
+        End If
+        If CheckBox3.Checked = True Then
+            str &= CheckBox3.Text
+            str &= " "
+        End If
+        If CheckBox4.Checked = True Then
+            str &= TextBox1.Text
+            str &= " "
+        End If
+        If str <> Nothing Then
+            MsgBox(str + vbLf + "Thank you")
+        End If
+
+    End Sub
+    Private Sub CheckBox4_CheckedChanged(sender As Object,
+      e As EventArgs) Handles CheckBox4.CheckedChanged
+        Label1.Visible = True
+        TextBox1.Visible = True
+    End Sub
 End Class
