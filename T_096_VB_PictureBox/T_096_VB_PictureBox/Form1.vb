e6480ba564fe2b7b5dde13cd0d3acda2698743f5
kagaya25 <32299866+kagaya25@users.noreply.github.com>
2018-05-02 02:37:26 +0900

    Done

--- T_096_VB_PictureBox/T_096_VB_PictureBox/Form1.vb
+++ T_096_VB_PictureBox/T_096_VB_PictureBox/Form1.vb
@@ -1,3 +1,11 @@
-﻿Public Class Form1
+﻿
+'VB.Net -PictureBox Control
+'The PictureBox control Is used For displaying images On the form. The Image Property Of the control allows you To Set an image both at design time Or at run time.
+
+'Let's create a picture box by dragging a PictureBox control from the Toolbox and dropping it on the form.
+
+
+
+Public Class Form1
 
 End Class
