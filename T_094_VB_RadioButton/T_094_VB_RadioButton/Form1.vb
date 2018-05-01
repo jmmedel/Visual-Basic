04219a290529bdc34ed20b4fced8548115870fdf
kagaya25 <32299866+kagaya25@users.noreply.github.com>
2018-05-02 01:49:40 +0900

    Done
    Done

--- T_094_VB_RadioButton/T_094_VB_RadioButton/Form1.vb
+++ T_094_VB_RadioButton/T_094_VB_RadioButton/Form1.vb
@@ -5,6 +5,67 @@
 
 'Let's create three radio buttons by dragging RadioButton controls from the Toolbox and dropping on the form.
 
-Public Class Form1
+'The Checked Property Of the radio button Is used To Set the state Of a radio button. You can display text, image Or both On radio button control. You can also change the appearance Of the radio button control by Using the Appearance Property.
 
+'Properties of the RadioButton Control
+'The following are some Of the commonly used properties Of the RadioButton control
+
+'S.N  Property	Description
+'1   Appearance	Gets Or sets a value determining the appearance of the radio button.
+'2   AutoCheck	Gets Or sets a value indicating whether the Checked value And the appearance of the control automatically change when the control Is clicked.
+'3   CheckAlign	Gets Or sets the location of the check box portion of the radio button.
+'4   Checked	Gets Or sets a value indicating whether the control Is checked.
+'5   Text	Gets Or sets the caption for a radio button.
+'6   TabStop	Gets Or sets a value indicating whether a user can give focus to the RadioButton control using the TAB key.
+'Methods of the RadioButton Control
+'The following are some Of the commonly used methods Of the RadioButton control
+
+'S.N  Method Name & Description
+'1   
+'PerformClick
+
+'Generates a Click Event For the control, simulating a click by a user.
+
+'Events of the RadioButton Control
+'The following are some Of the commonly used events Of the RadioButton control
+
+'S.N  Event	Description
+'1   AppearanceChanged	Occurs when the value of the Appearance property of the RadioButton control Is changed.
+'2   CheckedChanged	Occurs when the value of the Checked property of the RadioButton control Is changed.
+'Consult Microsoft documentation For detailed list Of properties, methods And events Of the RadioButton control.
+
+'Example
+'In the following example, let us create two groups of radio buttons And use their CheckedChanged events for changing the BackColor And ForeColor property of the form.
+
+Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ' Set the caption bar text of the form.  
+        Me.Text = "tutorialspont.com"
+    End Sub
+
+    Private Sub RadioButton1_CheckedChanged(sender As Object,
+       e As EventArgs) Handles RadioButton1.CheckedChanged
+        Me.BackColor = Color.Red
+    End Sub
+    Private Sub RadioButton2_CheckedChanged(sender As Object,
+      e As EventArgs) Handles RadioButton2.CheckedChanged
+        Me.BackColor = Color.Green
+    End Sub
+    Private Sub RadioButton3_CheckedChanged(sender As Object,
+      e As EventArgs) Handles RadioButton3.CheckedChanged
+        Me.BackColor = Color.Blue
+    End Sub
+
+    Private Sub RadioButton4_CheckedChanged(sender As Object,
+      e As EventArgs) Handles RadioButton4.CheckedChanged
+        Me.ForeColor = Color.Black
+    End Sub
+    Private Sub RadioButton5_CheckedChanged(sender As Object,
+       e As EventArgs) Handles RadioButton5.CheckedChanged
+        Me.ForeColor = Color.White
+    End Sub
+    Private Sub RadioButton6_CheckedChanged(sender As Object,
+      e As EventArgs) Handles RadioButton6.CheckedChanged
+        Me.ForeColor = Color.Red
+    End Sub
 End Class
---
+++ T_095_VB_Checkbox/T_095_VB_Checkbox/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("T_095_VB_Checkbox")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("T_095_VB_Checkbox")>
+<Assembly: AssemblyCopyright("Copyright ©  2018")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("46264435-a211-4b92-8a87-9d0625f53c31")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
