82c48b91a05ddd113da88d07ddc69068c7871d4b
kagaya25 <jm.kagaya@gmail.com>
2018-05-02 10:32:20 +0900

    Done
    Done

--- VB_Sandbox/Visual_Basic_Sandbox2/S-02-01-RentalCost/S-02-01-RentalCost/Form1.vb
+++ VB_Sandbox/Visual_Basic_Sandbox2/S-02-01-RentalCost/S-02-01-RentalCost/Form1.vb
@@ -1,12 +1,13 @@
 ﻿Public Class FrmRentalCost
-  Private Sub btnDisplayRates_Click(sender As Object, e As EventArgs) Handles btnDisplayRates.Click
-    lstRentalRates.Items.Add("Price of Equipment" & vbTab & vbTab & "Half Day" & vbTab & "Full Day")
-    lstRentalRates.Items.Add("1. Rug Cleaner" & vbTab & vbTab & "$16.00" & vbTab & "$24.00")
-    lstRentalRates.Items.Add("2. Lawn Mower" & vbTab & vbTab & "$12.00" & vbTab & "$18.00")
-    lstRentalRates.Items.Add("3. Paint sprayer" & vbTab & vbTab & "$20.00" & vbTab & "$30.00")
-  End Sub
+    Private Sub btnDisplayRates_Click(sender As Object, e As EventArgs) Handles btnDisplayRates.Click
+        lstRentalRates.Items.Clear()
+        lstRentalRates.Items.Add("Price of Equipment" & vbTab & vbTab & "Half Day" & vbTab & "Full Day")
+        lstRentalRates.Items.Add("1. Rug Cleaner" & vbTab & vbTab & "$16.00" & vbTab & "$24.00")
+        lstRentalRates.Items.Add("2. Lawn Mower" & vbTab & vbTab & "$12.00" & vbTab & "$18.00")
+        lstRentalRates.Items.Add("3. Paint sprayer" & vbTab & vbTab & "$20.00" & vbTab & "$30.00")
+    End Sub
 
-  Private Sub btnDisplayBill_Click(sender As Object, e As EventArgs) Handles btnDisplayBill.Click
+    Private Sub btnDisplayBill_Click(sender As Object, e As EventArgs) Handles btnDisplayBill.Click
     lstBill.Items.Clear()
 
     If (txtItem.Text = "1" Or txtItem.Text = "2" Or txtItem.Text = "3") And (txtDuration.Text.ToUpper() = "H" Or txtDuration.Text.ToUpper() = "F")
