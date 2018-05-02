62080e7f2dfa7b5ca045a7fed21d3fe77ed883e6
kagaya25 <jm.kagaya@gmail.com>
2018-05-02 10:21:51 +0900

    Done
    Done

--- VB_Sandbox/Visual_Basic_Sandbox2/S-01-03-ChangeForDollar/S-01-03-ChangeForDollar/S-01-03-ChangeForDollar/Form1.vb
+++ VB_Sandbox/Visual_Basic_Sandbox2/S-01-03-ChangeForDollar/S-01-03-ChangeForDollar/S-01-03-ChangeForDollar/Form1.vb
@@ -1,27 +1,28 @@
 ﻿Public Class FrmChangeForDollar
-  Private Sub btnCalculate_Click(sender As Object, e As EventArgs) Handles btnCalculate.Click
-    Dim amount, cents25,cents10,cents5,cents1 as Double
+    Private Sub btnCalculate_Click(sender As Object, e As EventArgs) Handles btnCalculate.Click
+        'this is the variable'
+        Dim amount, cents25, cents10, cents5, cents1 As Double
+        'convert todouble'
+        amount = CDbl(txtAmount.Text)
 
-    amount = CDbl(txtAmount.Text)
+        cents25 = Int(amount / 25)
+        txt25.Text = cents25
 
-    cents25 =Int(amount/25)
-    txt25.Text = cents25
+        amount = amount - (25 * cents25)
 
-    amount = amount - (25 * cents25)
+        cents10 = Int(amount / 10)
+        txt10.Text = cents10
 
-    cents10 = Int(amount / 10)
-    txt10.Text = cents10
+        amount = amount - (10 * cents10)
 
-    amount = amount -(10 * cents10)
+        cents5 = Int(amount / 5)
+        txt5.Text = cents5
 
-    cents5 = Int(amount / 5)
-    txt5.Text = cents5
+        amount = amount - (5 * cents5)
 
-    amount = amount - (5 * cents5)
-
-    cents1 = amount
-    txt1.Text = cents1
+        cents1 = amount
+        txt1.Text = cents1
 
 
-  End Sub
+    End Sub
 End Class
