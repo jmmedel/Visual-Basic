b31fea2e1626abd075e125aaaeacd27937fe379f
kagaya25 <jm.kagaya@gmail.com>
2018-05-10 13:50:02 +0900

    Done
    Done

--- T_100_VB_TreeView/T_100_VB_TreeView/Form1.vb
+++ T_100_VB_TreeView/T_100_VB_TreeView/Form1.vb
@@ -1,3 +1,10 @@
-﻿Public Class Form1
+﻿
+'VB.Net -TreeView Control
+'The TreeView control Is used To display hierarchical representations Of items similar To the ways the files And folders are displayed In the left pane Of the Windows Explorer. Each node may contain one Or more child nodes.
+
+'Let's click on a TreeView control from the Toolbox and place it on the form.
+
+
+Public Class Form1
 
 End Class
