d9e0a49b7d73984c4f2b00b1a93d7bd5f91cb0aa
kagaya25 <jm.kagaya@gmail.com>
2018-05-17 13:23:48 +0900

    Done
    Done

--- T_106_VB_SaveFileDialog_Controls/T_106_VB_SaveFileDialog_Controls/Form1.Designer.vb
+++ T_106_VB_SaveFileDialog_Controls/T_106_VB_SaveFileDialog_Controls/Form1.Designer.vb
@@ -22,9 +22,55 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.RichTextBox1 = New System.Windows.Forms.RichTextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.SaveFileDialog1 = New System.Windows.Forms.SaveFileDialog()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(31, 31)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(159, 12)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "We appreciate your comments"
+        '
+        'RichTextBox1
+        '
+        Me.RichTextBox1.Location = New System.Drawing.Point(33, 57)
+        Me.RichTextBox1.Name = "RichTextBox1"
+        Me.RichTextBox1.Size = New System.Drawing.Size(179, 151)
+        Me.RichTextBox1.TabIndex = 1
+        Me.RichTextBox1.Text = ""
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(252, 81)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 60)
+        Me.Button1.TabIndex = 2
+        Me.Button1.Text = "Save Comment"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(366, 261)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.RichTextBox1)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
         Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
 
+    Friend WithEvents Label1 As Label
+    Friend WithEvents RichTextBox1 As RichTextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents SaveFileDialog1 As SaveFileDialog
 End Class
--- T_106_VB_SaveFileDialog_Controls/T_106_VB_SaveFileDialog_Controls/Form1.vb
+++ T_106_VB_SaveFileDialog_Controls/T_106_VB_SaveFileDialog_Controls/Form1.vb
@@ -1,3 +1,53 @@
-﻿Public Class Form1
+﻿
+'VB.Net -SaveFileDialog Control
+'The SaveFileDialog control prompts the user To Select a location For saving a file And allows the user To specify the name Of the file To save data. The SaveFileDialog control Class Inherits from the abstract Class FileDialog.
+
+'Following Is the Save File dialog box
+
+'Properties of the SaveFileDialog Control
+'The following are some Of the commonly used properties Of the SaveFileDialog control
+
+'S.N  Property	Description
+'1   AddExtension	Gets Or sets a value indicating whether the dialog box automatically adds an extension to a file name if the user omits the extension.
+'2   CheckFileExists	Gets Or sets a value indicating whether the dialog box displays a warning if the user specifies a file name that does Not exist.
+'3   CheckPathExists	Gets Or sets a value indicating whether the dialog box displays a warning if the user specifies a path that does Not exist.
+'4   CreatePrompt	Gets Or sets a value indicating whether the dialog box prompts the user for permission to create a file if the user specifies a file that does Not exist.
+'5   DefaultExt	Gets Or sets the default file name extension.
+'6   DereferenceLinks	Gets Or sets a value indicating whether the dialog box returns the location of the file referenced by the shortcut Or whether it returns the location of the shortcut (.lnk).
+'7   FileName	Gets Or sets a string containing the file name selected in the file dialog box.
+'8   FileNames	Gets the file names of all selected files in the dialog box.
+'9   Filter	Gets Or sets the current file name filter string, which determines the choices that appear in the "Save as file type" Or "Files of type" box in the dialog box.
+'10  FilterIndex	Gets Or sets the index of the filter currently selected in the file dialog box.
+'11  InitialDirectory	Gets Or sets the initial directory displayed by the file dialog box.
+'12  OverwritePrompt	Gets Or sets a value indicating whether the Save As dialog box displays a warning if the user specifies a file name that already exists.
+'13  RestoreDirectory	Gets Or sets a value indicating whether the dialog box restores the current directory before closing.
+'14  ShowHelp	Gets Or sets a value indicating whether the Help button Is displayed in the file dialog box.
+'15  SupportMultiDottedExtensions	Gets Or sets whether the dialog box supports displaying And saving files that have multiple file name extensions.
+'16  Title	Gets Or sets the file dialog box title.
+'17  ValidateNames	Gets Or sets a value indicating whether the dialog box accepts only valid Win32 file names.
+'Methods of the SaveFileDialog Control
+'The following are some Of the commonly used methods Of the SaveFileDialog control
+
+'S.N  Method Name & Description
+'1   
+'OpenFile
+
+'Opens the file With read/write permission.
+
+'2   
+'Reset
+
+'Resets all dialog box options To their Default values.
+
+'Example
+'In this example, let's save the text entered into a rich text box by the user using the save file dialog box. Take the following steps:
+
+'Drag And drop a Label control, a RichTextBox control, a Button control And a SaveFileDialog control on the form.
+
+'Set the Text Property Of the label And the button control To 'We appreciate your comments' and 'Save Comments', respectively.
+
+'Double-click the Save Comments button And modify the code of the Click event as shown
+
+Public Class Form1
 
 End Class
