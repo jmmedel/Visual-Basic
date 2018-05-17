1355af87353a4636e9ab567654b6127c6d65dda2
kagaya25 <jm.kagaya@gmail.com>
2018-05-17 12:11:35 +0900

    Done
    Done

--- T_103_VB_ColorDialog_Controls/T_103_VB_ColorDialog_Controls/Form1.Designer.vb
+++ T_103_VB_ColorDialog_Controls/T_103_VB_ColorDialog_Controls/Form1.Designer.vb
@@ -22,9 +22,44 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.ColorDialog1 = New System.Windows.Forms.ColorDialog()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(91, 151)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(108, 23)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Change Color"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(89, 75)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(110, 12)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Gibe me a new color"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(284, 261)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
         Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
 
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents ColorDialog1 As ColorDialog
 End Class
--- T_103_VB_ColorDialog_Controls/T_103_VB_ColorDialog_Controls/Form1.vb
+++ T_103_VB_ColorDialog_Controls/T_103_VB_ColorDialog_Controls/Form1.vb
@@ -1,3 +1,62 @@
-﻿Public Class Form1
+﻿
+'VB.Net -ColorDialog Control
+'The ColorDialog control Class represents a common dialog box that displays available colors along With controls that enable the user To define custom colors. It lets the user Select a color.
 
+'The main Property Of the ColorDialog control Is Color, which returns a Color Object.
+
+'Following Is the Color dialog box
+'    Properties of the ColorDialog Control
+'The following are some Of the commonly used properties Of the ColorDialog control
+
+'S.N  Property	Description
+'1   AllowFullOpen	Gets Or sets a value indicating whether the user can use the dialog box to define custom colors.
+'2   AnyColor	Gets Or sets a value indicating whether the dialog box displays all available colors in the set of basic colors.
+'3   CanRaiseEvents	Gets a value indicating whether the component can raise an event.
+'4   Color	Gets Or sets the color selected by the user.
+'5   CustomColors	Gets Or sets the set of custom colors shown in the dialog box.
+'6   FullOpen	Gets Or sets a value indicating whether the controls used to create custom colors are visible when the dialog box Is opened
+'7   ShowHelp	Gets Or sets a value indicating whether a Help button appears in the color dialog box.
+'8   SolidColorOnly	Gets Or sets a value indicating whether the dialog box will restrict users to selecting solid colors only.
+'Methods of the ColorDialog Control
+'The following are some Of the commonly used methods Of the ColorDialog control
+
+'S.N  Method Name & Description
+'1   
+'Reset
+
+'Resets all options To their Default values, the last selected color To black, And the custom colors To their Default values.
+
+'2   
+'RunDialog
+
+'When overridden in a derived class, specifies a common dialog box.
+
+'3   
+'ShowDialog
+
+'Runs a common dialog box With a Default owner.
+
+'Events of the ColorDialog Control
+'The following are some Of the commonly used events Of the ColorDialog control
+
+'S.N  Event	Description
+'1   HelpRequest	Occurs when the user clicks the Help button on a common dialog box.
+'Example
+'In this example, let's change the forecolor of a label control using the color dialog box. Take the following steps:
+
+'Drag And drop a label control, a button control And a ColorDialog control on the form.
+
+'Set the Text Property Of the label And the button control To 'Give me a new Color' and 'Change Color', respectively.
+
+'Change the font Of the label As per your likings.
+
+'Double-click the Change Color button And modify the code of the Click event.
+
+
+Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If ColorDialog1.ShowDialog <> Windows.Forms.DialogResult.Cancel Then
+            Label1.ForeColor = ColorDialog1.Color
+        End If
+    End Sub
 End Class
