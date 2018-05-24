ba980d6c8a8125741a1f86f6395dd1c1178f85d5
kagaya25 <jm.kagaya@gmail.com>
2018-05-24 14:03:50 +0900

    Done
    Done

--- Kadai_VB/kadai13/kadai13/Form1.vb
+++ Kadai_VB/kadai13/kadai13/Form1.vb
@@ -3,7 +3,7 @@
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
         If (TextBox1.Text <> "") Then
             Dim evennumber As Integer = TextBox1.Text
-
+            TextBox1.Clear()
             total += evennumber
             Label2.Text = "愚数の合計は" & total & "です"
         Else
