3326d8629560061bf585e52dfb5f8b45233e2654
kagaya25 <jm.kagaya@gmail.com>
2018-05-24 14:44:14 +0900

    Done
    Done

--- Kadai_VB/kadai15/kadai14/Form1.vb
+++ Kadai_VB/kadai15/kadai14/Form1.vb
@@ -3,8 +3,10 @@
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
         Label2.Text = ""
         Dim number As Integer = CInt(TextBox1.Text)
-        For x As Integer = 3 To number Step 3
-            Label2.Text += x.ToString + " "
+        For x As Integer = 1 To number
+            If x Mod 3 <> 0 Then
+                Label2.Text += x.ToString + " "
+            End If
 
         Next
 
@@ -14,8 +16,10 @@
     Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
         Label2.Text = ""
         Dim number As Integer = CInt(TextBox1.Text)
-        For x As Integer = 7 To number Step 7
-            Label2.Text += x.ToString + " "
+        For x As Integer = 1 To number
+            If x Mod 7 <> 0 Then
+                Label2.Text += x.ToString + " "
+            End If
 
         Next
 
@@ -24,14 +28,20 @@
     Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
         Label2.Text = ""
         Dim number As Integer = CInt(TextBox1.Text)
-        For x As Integer = 3 To number Step 3
-            Label2.Text += x.ToString + " "
+        For x As Integer = 1 To number
+
+            If x Mod 3 <> 0 Then
+                Label2.Text += x.ToString + " "
+            End If
+
+
 
         Next
-
-        For x As Integer = 7 To number Step 7
-            Label2.Text += x.ToString + " "
-
+        Label2.Text += "     "
+        For x As Integer = 1 To number
+            If x Mod 7 <> 0 Then
+                Label2.Text += x.ToString + " "
+            End If
         Next
 
     End Sub
@@ -40,18 +50,11 @@
         Label2.Text = ""
 
         Dim number As Integer = CInt(TextBox1.Text)
-        For x As Integer = 3 To number Step 3
+        For x As Integer = 1 To number
 
-
-            For y As Integer = 7 To number Step 7
-                If x = y Then
-                    Label2.Text += x.ToString + " "
-                Else
-                    Label2.Text += ""
-                End If
-
-            Next
-
+            If x Mod 3 <> 0 And x Mod 7 <> 0 Then
+                Label2.Text += x.ToString + " "
+            End If
         Next
 
 
