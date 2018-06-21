a194a784eaf7287a1b00a2b66b3339b97c889b32
kagaya25 <jm.kagaya@gmail.com>
2018-06-22 01:48:17 +0900

    Done
    Done

--- Kadai_VB/kadai16/kadai16/Form1.Designer.vb
+++ Kadai_VB/kadai16/kadai16/Form1.Designer.vb
@@ -24,6 +24,7 @@ Partial Class Form1
     Private Sub InitializeComponent()
         Me.Label1 = New System.Windows.Forms.Label()
         Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
         Me.SuspendLayout()
         '
         'Label1
@@ -41,18 +42,28 @@ Partial Class Form1
         Me.Button1.Name = "Button1"
         Me.Button1.Size = New System.Drawing.Size(75, 23)
         Me.Button1.TabIndex = 1
-        Me.Button1.Text = "Button1"
+        Me.Button1.Text = "表示する"
         Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(155, 132)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 2
+        Me.Button2.Text = "リセット"
+        Me.Button2.UseVisualStyleBackColor = True
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(287, 207)
+        Me.Controls.Add(Me.Button2)
         Me.Controls.Add(Me.Button1)
         Me.Controls.Add(Me.Label1)
         Me.Name = "Form1"
-        Me.Text = "Form1"
+        Me.Text = "0H01007"
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
@@ -60,4 +71,5 @@ Partial Class Form1
 
     Friend WithEvents Label1 As Label
     Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
 End Class
--- Kadai_VB/kadai16/kadai16/Form1.vb
+++ Kadai_VB/kadai16/kadai16/Form1.vb
@@ -1,11 +1,8 @@
 ﻿Public Class Form1
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-        Dim i As Integer
-        Label1.Alignment = 2
-        Label1 = ""
-        For i = 1 To 9 Step 2
-            Text1 = Text1 & String(i, "*") & vbCrLf
-        Next
+        Label1.Font = New Font("MS Gothic", 12)
+        Label1.ForeColor = Color.Red
+        Label1.Text &= Environment.NewLine
 
     End Sub
 End Class
