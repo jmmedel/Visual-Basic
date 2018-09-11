992a043dd7bec370c943df4fc6ab95741ec9f2b4
kagaya25 <jm.kagaya@gmail.com>
2018-09-11 11:25:39 +0900

    Done
    Done

--- T_112_VB_Event_Handling_Data_type/T_112_VB_Event_Handling_Data_type/Form1.Designer.vb
+++ T_112_VB_Event_Handling_Data_type/T_112_VB_Event_Handling_Data_type/Form1.Designer.vb
@@ -22,10 +22,118 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.txtID = New System.Windows.Forms.TextBox()
+        Me.txtName = New System.Windows.Forms.TextBox()
+        Me.txtAddress = New System.Windows.Forms.TextBox()
+        Me.TxtAge = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(26, 58)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(71, 12)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Customer ID:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(26, 102)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(34, 12)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Name"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(26, 151)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(47, 12)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Address"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(26, 198)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(25, 12)
+        Me.Label4.TabIndex = 3
+        Me.Label4.Text = "Age"
+        '
+        'txtID
+        '
+        Me.txtID.Location = New System.Drawing.Point(115, 51)
+        Me.txtID.Name = "txtID"
+        Me.txtID.Size = New System.Drawing.Size(100, 19)
+        Me.txtID.TabIndex = 4
+        '
+        'txtName
+        '
+        Me.txtName.Location = New System.Drawing.Point(115, 99)
+        Me.txtName.Name = "txtName"
+        Me.txtName.Size = New System.Drawing.Size(100, 19)
+        Me.txtName.TabIndex = 5
+        '
+        'txtAddress
+        '
+        Me.txtAddress.Location = New System.Drawing.Point(115, 144)
+        Me.txtAddress.Name = "txtAddress"
+        Me.txtAddress.Size = New System.Drawing.Size(100, 19)
+        Me.txtAddress.TabIndex = 6
+        '
+        'TxtAge
+        '
+        Me.TxtAge.Location = New System.Drawing.Point(115, 191)
+        Me.TxtAge.Name = "TxtAge"
+        Me.TxtAge.Size = New System.Drawing.Size(100, 19)
+        Me.TxtAge.TabIndex = 7
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(115, 244)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 8
+        Me.Button1.Text = "Submit"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.ClientSize = New System.Drawing.Size(327, 450)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.TxtAge)
+        Me.Controls.Add(Me.txtAddress)
+        Me.Controls.Add(Me.txtName)
+        Me.Controls.Add(Me.txtID)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
         Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
 
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents txtID As TextBox
+    Friend WithEvents txtName As TextBox
+    Friend WithEvents txtAddress As TextBox
+    Friend WithEvents TxtAge As TextBox
+    Friend WithEvents Button1 As Button
 End Class
--- T_112_VB_Event_Handling_Data_type/T_112_VB_Event_Handling_Data_type/Form1.vb
+++ T_112_VB_Event_Handling_Data_type/T_112_VB_Event_Handling_Data_type/Form1.vb
@@ -1,3 +1,24 @@
 ﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub txtID_KeyUP(sender As Object, e As KeyEventArgs) Handles txtID.KeyUp
+
+        If (Not Char.IsNumber(ChrW(e.KeyCode))) Then
+            MessageBox.Show("Enter numbers for your Customer ID")
+            txtID.Text = " "
+        End If
+    End Sub
+
+    Private Sub txtAge_KeyUP(sender As Object, e As KeyEventArgs) _
+   Handles TxtAge.KeyUp
+
+        If (Not Char.IsNumber(ChrW(e.KeyCode))) Then
+            MessageBox.Show("Enter numbers for age")
+            TxtAge.Text = " "
+        End If
+    End Sub
 
 End Class
+
