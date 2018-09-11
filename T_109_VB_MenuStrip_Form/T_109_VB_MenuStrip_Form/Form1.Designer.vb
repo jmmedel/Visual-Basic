5104b987377db5618bc49f94428df16c579a00c8
kagaya25 <jm.kagaya@gmail.com>
2018-09-11 10:32:14 +0900

    Done
    Done

--- T_109_VB_MenuStrip_Form/T_109_VB_MenuStrip_Form/Form1.Designer.vb
+++ T_109_VB_MenuStrip_Form/T_109_VB_MenuStrip_Form/Form1.Designer.vb
@@ -22,9 +22,91 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.FileToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.EditToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ViewToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.SaveToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.NewToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.OpenToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.SaveToolStripMenuItem1 = New System.Windows.Forms.ToolStripMenuItem()
+        Me.MenuStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.FileToolStripMenuItem, Me.EditToolStripMenuItem, Me.ViewToolStripMenuItem, Me.SaveToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(284, 24)
+        Me.MenuStrip1.TabIndex = 0
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'FileToolStripMenuItem
+        '
+        Me.FileToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.NewToolStripMenuItem, Me.OpenToolStripMenuItem, Me.SaveToolStripMenuItem1})
+        Me.FileToolStripMenuItem.Name = "FileToolStripMenuItem"
+        Me.FileToolStripMenuItem.Size = New System.Drawing.Size(39, 20)
+        Me.FileToolStripMenuItem.Text = "File"
+        '
+        'EditToolStripMenuItem
+        '
+        Me.EditToolStripMenuItem.Name = "EditToolStripMenuItem"
+        Me.EditToolStripMenuItem.Size = New System.Drawing.Size(41, 20)
+        Me.EditToolStripMenuItem.Text = "Edit"
+        '
+        'ViewToolStripMenuItem
+        '
+        Me.ViewToolStripMenuItem.Name = "ViewToolStripMenuItem"
+        Me.ViewToolStripMenuItem.Size = New System.Drawing.Size(47, 20)
+        Me.ViewToolStripMenuItem.Text = "View"
+        '
+        'SaveToolStripMenuItem
+        '
+        Me.SaveToolStripMenuItem.Name = "SaveToolStripMenuItem"
+        Me.SaveToolStripMenuItem.Size = New System.Drawing.Size(48, 20)
+        Me.SaveToolStripMenuItem.Text = "Save"
+        '
+        'NewToolStripMenuItem
+        '
+        Me.NewToolStripMenuItem.Name = "NewToolStripMenuItem"
+        Me.NewToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.NewToolStripMenuItem.Text = "New"
+        '
+        'OpenToolStripMenuItem
+        '
+        Me.OpenToolStripMenuItem.Name = "OpenToolStripMenuItem"
+        Me.OpenToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.OpenToolStripMenuItem.Text = "Open"
+        '
+        'SaveToolStripMenuItem1
+        '
+        Me.SaveToolStripMenuItem1.Name = "SaveToolStripMenuItem1"
+        Me.SaveToolStripMenuItem1.Size = New System.Drawing.Size(180, 22)
+        Me.SaveToolStripMenuItem1.Text = "Save"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(284, 261)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.Name = "Form1"
         Me.Text = "Form1"
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
 
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents FileToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents NewToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents OpenToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents SaveToolStripMenuItem1 As ToolStripMenuItem
+    Friend WithEvents EditToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ViewToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents SaveToolStripMenuItem As ToolStripMenuItem
 End Class
--- T_109_VB_MenuStrip_Form/T_109_VB_MenuStrip_Form/Form1.vb
+++ T_109_VB_MenuStrip_Form/T_109_VB_MenuStrip_Form/Form1.vb
@@ -1,3 +1,85 @@
-﻿Public Class Form1
+﻿
+'VB.Net -MenuStrip Control
+'Advertisements
+' Previous Page Next Page  
+'The MenuStrip control represents the container For the menu Structure.
 
+'The MenuStrip control works As the top-level container For the menu Structure. The ToolStripMenuItem Class And the ToolStripDropDownMenu Class provide the functionalities To create menu items, Sub menus And drop-down menus.
+
+'The following diagram shows adding a MenuStrip control On the form −
+
+'VB.Net MenuStrip Control
+'Properties of the MenuStrip Control
+'The following are some Of the commonly used properties Of the MenuStrip control −
+
+'Sr.No.Property & Description
+'1   
+'CanOverflow
+
+'Gets Or sets a value indicating whether the MenuStrip supports overflow functionality.
+
+'2   
+'GripStyle
+
+'Gets Or sets the visibility of the grip used to reposition the control.
+
+'3   
+'MdiWindowListItem
+
+'Gets Or sets the ToolStripMenuItem that Is used to display a list of Multiple-document interface (MDI) child forms.
+
+'4   
+'ShowItemToolTips
+
+'Gets Or sets a value indicating whether ToolTips are shown for the MenuStrip.
+
+'5   
+'Stretch
+
+'Gets Or sets a value indicating whether the MenuStrip stretches from end to end in its container.
+
+'Events of the MenuStrip Control
+'The following are some Of the commonly used events Of the MenuStrip control −
+
+'Sr.No.Event & Description
+'1   
+'MenuActivate
+
+'Occurs when the user accesses the menu with the keyboard Or mouse.
+
+'2   
+'MenuDeactivate
+
+'Occurs when the MenuStrip Is deactivated.
+
+'Example
+'In this example, let us add menu And sub-menu items.
+
+'Take the following steps −
+
+'Drag And drop Or Double click On a MenuStrip control, to add it to the form.
+
+'Click the Type Here text To open a text box And enter the names Of the menu items Or Sub-menu items you want. When you add a Sub-menu, another text box With 'Type Here' text opens below it.
+
+'Complete the menu Structure shown In the diagram above.
+
+'Add a Sub menu Exit under the File menu.
+
+'VB.Net MenuStrip Example
+'Double-Click the Exit menu created And add the following code to the Click event of ExitToolStripMenuItem −
+
+'Private Sub ExitToolStripMenuItem_Click(sender As Object, e As EventArgs) _
+'   Handles ExitToolStripMenuItem.Click
+'    End
+'End Sub
+'When the above code Is executed And run using Start button available at the Microsoft Visual Studio tool bar, it will show the following window
+
+'VB.Net MenuStrip Example
+'Click on the File -> Exit to exit from the application −
+
+
+Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
 End Class
