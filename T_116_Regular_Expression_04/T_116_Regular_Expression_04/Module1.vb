0621b2351b55f40207cae986f1aac48258639bf5
kagaya25 <jm.kagaya@gmail.com>
2018-09-11 11:56:22 +0900

    Done
    Done

---
+++ T_116_Regular_Expression_04/T_116_Regular_Expression_04/Module1.vb
@@ -0,0 +1,17 @@
+﻿Imports System.Text.RegularExpressions
+Module Module1
+    Sub Main()
+        Dim input As String = "Hello    World   "
+        Dim pattern As String = "\\s+"
+        Dim replacement As String = " "
+        Dim rgx As Regex = New Regex(pattern)
+        Dim result As String = rgx.Replace(input, replacement)
+
+        Console.WriteLine("Original String: {0}", input)
+        Console.WriteLine("Replacement String: {0}", result)
+        Console.ReadKey()
+    End Sub
+
+    'Original String: Hello   World
+    'Replacement String: Hello World
+End Module
---
+++ T_116_Regular_Expression_04/T_116_Regular_Expression_04/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("T_116_Regular_Expression_04")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("T_116_Regular_Expression_04")>
+<Assembly: AssemblyCopyright("Copyright ©  2018")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("7b7dd804-7631-4b27-9f9a-90af5fb4049d")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
