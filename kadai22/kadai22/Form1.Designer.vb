3c082fe18b7ab083f415063a3fe9c2f1b9518388
kagaya25 <jm.kagaya@gmail.com>
2018-09-11 10:25:48 +0900

    Done
    done

--- kadai22/kadai22/Form1.Designer.vb
+++ kadai22/kadai22/Form1.Designer.vb
@@ -29,53 +29,58 @@ Partial Class Form1
         Me.Button1 = New System.Windows.Forms.Button()
         Me.Label2 = New System.Windows.Forms.Label()
         Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
         Me.SuspendLayout()
         '
         'Label1
         '
         Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(40, 40)
+        Me.Label1.Font = New System.Drawing.Font("MS UI Gothic", 22.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
+        Me.Label1.Location = New System.Drawing.Point(12, 28)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(38, 12)
+        Me.Label1.Size = New System.Drawing.Size(176, 30)
         Me.Label1.TabIndex = 0
-        Me.Label1.Text = "Label1"
+        Me.Label1.Text = "年齢を入れる"
         '
         'TextBox1
         '
-        Me.TextBox1.Location = New System.Drawing.Point(154, 40)
+        Me.TextBox1.Location = New System.Drawing.Point(203, 39)
         Me.TextBox1.Name = "TextBox1"
-        Me.TextBox1.Size = New System.Drawing.Size(100, 19)
+        Me.TextBox1.Size = New System.Drawing.Size(131, 19)
         Me.TextBox1.TabIndex = 1
         '
         'RadioButton1
         '
         Me.RadioButton1.AutoSize = True
+        Me.RadioButton1.Font = New System.Drawing.Font("MS UI Gothic", 22.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
         Me.RadioButton1.Location = New System.Drawing.Point(42, 74)
         Me.RadioButton1.Name = "RadioButton1"
-        Me.RadioButton1.Size = New System.Drawing.Size(92, 16)
+        Me.RadioButton1.Size = New System.Drawing.Size(61, 34)
         Me.RadioButton1.TabIndex = 2
         Me.RadioButton1.TabStop = True
-        Me.RadioButton1.Text = "RadioButton1"
+        Me.RadioButton1.Text = "男"
         Me.RadioButton1.UseVisualStyleBackColor = True
         '
         'RadioButton2
         '
         Me.RadioButton2.AutoSize = True
+        Me.RadioButton2.Font = New System.Drawing.Font("MS UI Gothic", 22.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
         Me.RadioButton2.Location = New System.Drawing.Point(154, 74)
         Me.RadioButton2.Name = "RadioButton2"
-        Me.RadioButton2.Size = New System.Drawing.Size(92, 16)
+        Me.RadioButton2.Size = New System.Drawing.Size(61, 34)
         Me.RadioButton2.TabIndex = 3
         Me.RadioButton2.TabStop = True
-        Me.RadioButton2.Text = "RadioButton2"
+        Me.RadioButton2.Text = "女"
         Me.RadioButton2.UseVisualStyleBackColor = True
         '
         'Button1
         '
-        Me.Button1.Location = New System.Drawing.Point(276, 74)
+        Me.Button1.Font = New System.Drawing.Font("MS UI Gothic", 22.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
+        Me.Button1.Location = New System.Drawing.Point(247, 74)
         Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.Size = New System.Drawing.Size(104, 42)
         Me.Button1.TabIndex = 4
-        Me.Button1.Text = "Button1"
+        Me.Button1.Text = "表示"
         Me.Button1.UseVisualStyleBackColor = True
         '
         'Label2
@@ -83,23 +88,33 @@ Partial Class Form1
         Me.Label2.AutoSize = True
         Me.Label2.Location = New System.Drawing.Point(152, 122)
         Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(38, 12)
+        Me.Label2.Size = New System.Drawing.Size(0, 12)
         Me.Label2.TabIndex = 5
-        Me.Label2.Text = "Label2"
         '
         'TextBox2
         '
-        Me.TextBox2.Location = New System.Drawing.Point(42, 160)
+        Me.TextBox2.Location = New System.Drawing.Point(42, 168)
         Me.TextBox2.Multiline = True
         Me.TextBox2.Name = "TextBox2"
         Me.TextBox2.Size = New System.Drawing.Size(309, 108)
         Me.TextBox2.TabIndex = 6
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("MS UI Gothic", 22.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
+        Me.Label3.Location = New System.Drawing.Point(82, 122)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(217, 30)
+        Me.Label3.TabIndex = 7
+        Me.Label3.Text = "七五三の年齢↓"
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(376, 302)
+        Me.Controls.Add(Me.Label3)
         Me.Controls.Add(Me.TextBox2)
         Me.Controls.Add(Me.Label2)
         Me.Controls.Add(Me.Button1)
@@ -121,4 +136,5 @@ Partial Class Form1
     Friend WithEvents Button1 As Button
     Friend WithEvents Label2 As Label
     Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Label3 As Label
 End Class
--- kadai22/kadai22/Form1.vb
+++ kadai22/kadai22/Form1.vb
@@ -1,3 +1,20 @@
 ﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim boysAge As Integer = 2
+        Dim girlsAge As Integer = 4
+    End Sub
 
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim year As Integer
+        If TextBox1.Text = String.Empty Then
+            MessageBox.Show("Please enter the data to save", "Data Entry Error")
+            TextBox1.Focus()
+        Else
+            'Save your data here
+            year = TextBox1.Text
+        End If
+
+
+        TextBox2.Text = year
+    End Sub
 End Class
