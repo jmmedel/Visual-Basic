a5510a397344e5d1e20315c7e22072ec6b9c6b04
kagaya25 <jm.kagaya@gmail.com>
2018-09-19 14:46:07 +0900

    Done
    Done

--- kadai24/kadai24/Form1.vb
+++ kadai24/kadai24/Form1.vb
@@ -1,3 +1,24 @@
 ﻿Public Class Form1
 
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+
+
+
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        PictureBox1.Visible = False
+        PictureBox2.Visible = False
+        PictureBox3.Visible = False
+    End Sub
+
+
+
+    Private Sub CheckBox1_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBox1.CheckedChanged
+        PictureBox1.Visible = True
+    End Sub
+
+
 End Class
