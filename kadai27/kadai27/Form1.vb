be44cb2c2b228693b5ad736d4601d7f0089c507a
kagaya25 <jm.kagaya@gmail.com>
2018-09-20 14:30:11 +0900

    done
    done

--- kadai27/kadai27/Form1.vb
+++ kadai27/kadai27/Form1.vb
@@ -1,13 +1,43 @@
 ﻿Public Class Form1
-
-    Dim total As Integer
+    Dim total As Double = 0
+    Dim hoteltotal As Integer = 0
+    Dim optionaltotal As Integer = 0
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
         If (RadioButton1.Checked) Then
-
+            total += 125000
         ElseIf (RadioButton2.Checked) Then
-
+            total += 130000
         ElseIf (RadioButton3.Checked) Then
+            total += 135000
+        ElseIf (RadioButton4.Checked) Then
+            total += 140000
         End If
+
+        If (RadioButton5.Checked) Then
+
+        ElseIf (RadioButton6.Checked) Then
+
+            hoteltotal += 25000
+        End If
+
+
+        If (CheckBox1.Checked) Then
+            optionaltotal += 25000
+        ElseIf (CheckBox2.Checked) Then
+            optionaltotal += 6000
+        ElseIf (CheckBox3.Checked) Then
+            optionaltotal += 10000
+        ElseIf (CheckBox4.Checked) Then
+            optionaltotal += 15000
+
+        End If
+        total += optionaltotal + hoteltotal
+        MsgBox("合計" & total)
+        total = 0
+        optionaltotal = 0
+        hoteltotal = 0
+
+
     End Sub
 
 
---
+++ kadai28/kadai28/Form1.vb
@@ -0,0 +1,43 @@
+﻿Imports System.Threading
+Imports System.Threading.Tasks
+
+
+
+Public Class Form1
+    Private myTimer As System.Threading.Timer
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        Label1.Text = myTimer
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim myCallback As New System.Threading.TimerCallback(AddressOf Task1)
+
+        myTimer = New System.Threading.Timer(myCallback, Nothing, 100, 10000)
+
+    End Sub
+
+
+    Private Sub Task1(ByVal state As Object)
+
+        Try
+
+            Label1.Left -= 1
+
+            If Label1.Right <= Me.Left Then
+
+                Label1.Left = Me.Right
+
+            End If
+
+        Catch ex As Exception
+
+            MessageBox.Show(ex.Message)
+
+        End Try
+
+    End Sub
+
+End Class
