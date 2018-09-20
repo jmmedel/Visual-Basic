2c8e32566b8d706456538ddeabddbb7313c22c05
kagaya25 <jm.kagaya@gmail.com>
2018-09-20 10:11:51 +0900

    done
    done

--- kadai24/kadai24/Form1.vb
+++ kadai24/kadai24/Form1.vb
@@ -1,10 +1,22 @@
 ﻿Public Class Form1
+    Dim total As Integer = 0
+    Dim lunch As Integer = 850
+    Dim cake As Integer = 500
+    Dim coffe As Integer = 300
+    Dim cakeset As Integer = 650
 
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-
-
-
-
+        PictureBox1.Visible = False
+        PictureBox2.Visible = False
+        PictureBox3.Visible = False
+        CheckBox1.Checked = False
+        CheckBox2.Checked = False
+        CheckBox3.Checked = False
+        CheckBox1.Enabled = True
+        CheckBox2.Enabled = True
+        CheckBox3.Enabled = True
+        Label1.Text = "合計金額は" & total
+        total = 0
 
     End Sub
 
@@ -12,12 +24,37 @@
         PictureBox1.Visible = False
         PictureBox2.Visible = False
         PictureBox3.Visible = False
+
     End Sub
 
 
 
     Private Sub CheckBox1_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBox1.CheckedChanged
         PictureBox1.Visible = True
+        CheckBox1.Enabled = False
+        total += lunch
+    End Sub
+
+    Private Sub CheckBox2_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBox2.CheckedChanged
+        PictureBox2.Visible = True
+        CheckBox2.Enabled = False
+        If (CheckBox1.Checked) Then
+            total += cake - 200
+        Else
+            total += cake
+        End If
+
+
+    End Sub
+
+    Private Sub CheckBox3_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBox3.CheckedChanged
+        PictureBox3.Visible = True
+        CheckBox3.Enabled = False
+        If (CheckBox1.Checked) Then
+            total += coffe - 200
+        Else
+            total += coffe
+        End If
     End Sub
 
 
