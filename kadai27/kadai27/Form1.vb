ecc7e4c9e7227f80a7ec5e09ffe4e91b01f619e8
kagaya25 <jm.kagaya@gmail.com>
2018-09-20 13:41:08 +0900

    done
    Done

--- kadai27/kadai27/Form1.vb
+++ kadai27/kadai27/Form1.vb
@@ -1,7 +1,15 @@
 ﻿Public Class Form1
 
+    Dim total As Integer
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If (RadioButton1.Checked) Then
 
+        ElseIf (RadioButton2.Checked) Then
+
+        ElseIf (RadioButton3.Checked) Then
+        End If
     End Sub
+
+
 
 End Class
