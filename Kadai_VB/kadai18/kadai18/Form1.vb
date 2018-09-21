a08969c2728e99385218ca036def8fd099abb104
kagaya25 <jm.kagaya@gmail.com>
2018-09-21 13:23:29 +0900

    Done
    Done

--- Kadai_VB/kadai18/kadai18/Form1.vb
+++ Kadai_VB/kadai18/kadai18/Form1.vb
@@ -4,6 +4,77 @@
 
     Private Sub Button1_Click_1(sender As Object, e As EventArgs) Handles Button1.Click
 
+        If TextBox1.Text <> "" Then
+
+            Try
+                Dim number As Integer = CInt(TextBox1.Text)
+
+                If number < 0 Then
+                    TextBox2.AppendText("-")
+                End If
+
+
+                Select Case number
+                    Case number <= 999
+                        TextBox2.AppendText("九")
+                    Case number >= 800
+
+                    Case number >= 700
+
+                    Case number >= 600
+
+                    Case number >= 500
+
+                    Case number >= 400
+
+                    Case number >= 300
+
+                    Case number >= 200
+
+                    Case number >= 100
+
+                    Case number >= 90
+
+                    Case number >= 80
+
+                    Case number >= 70
+
+                    Case number >= 60
+
+                    Case number >= 50
+
+                    Case number >= 40
+
+                    Case number >= 30
+
+                    Case number >= 20
+
+                    Case number >= 10
+
+                    Case number >= 10
+
+                    Case number >= 10
+                    Case number >= 10
+                    Case number >= 10
+                    Case number >= 10
+                    Case number >= 10
+                    Case number >= 10
+                    Case number >= 10
+                    Case number >= 10
+
+                    Case number >= 10
+
+                End Select
+
+            Catch ex As Exception
+                MsgBox("整数値を入力してください")
+            End Try
+
+
+        End If
+
+
+
 
 
     End Sub
--- Kadai_VB/kadai19/kadai19/Form1.vb
+++ Kadai_VB/kadai19/kadai19/Form1.vb
@@ -1,3 +1,67 @@
 ﻿Public Class Form1
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Me.Close()
+    End Sub
 
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        If TextBox1.Text <> "" Then
+
+            Try
+                Dim months As Integer = CInt(TextBox1.Text)
+                If months >= 13 Then
+                    MsgBox(" 1-12 だけ整数でお願いします")
+                Else
+
+                    Select Case months
+                        Case 1
+                            Label2.Text = "陰暦では 「睦月」です"
+                            Label3.Text = "読み方は「むつき」です"
+
+                        Case 2
+                            Label2.Text = "陰暦では 「如月」です"
+                            Label3.Text = "読み方は「きさらぎ」です"
+                        Case 3
+                            Label2.Text = "陰暦では 「弥生」です"
+                            Label3.Text = "読み方は「やよい」です"
+                        Case 4
+                            Label2.Text = "陰暦では 「卯月」です"
+                            Label3.Text = "読み方は「うづき」です"
+                        Case 5
+                            Label2.Text = "陰暦では 「皐月」です"
+                            Label3.Text = "読み方は「さつき」です"
+                        Case 6
+                            Label2.Text = "陰暦では 「水無月」です"
+                            Label3.Text = "読み方は「みなづき」です"
+                        Case 7
+                            Label2.Text = "陰暦では 「文月」です"
+                            Label3.Text = "読み方は「ふみづき」です"
+                        Case 8
+                            Label2.Text = "陰暦では 「葉月」です"
+                            Label3.Text = "読み方は「はづき」です"
+                        Case 9
+                            Label2.Text = "陰暦では 「長月」です"
+                            Label3.Text = "読み方は「ながつき」です"
+                        Case 10
+                            Label2.Text = "陰暦では 「神無月」です"
+                            Label3.Text = "読み方は「かんなづ」です"
+                        Case 11
+                            Label2.Text = "陰暦では 「霜月」です"
+                            Label3.Text = "読み方は「しもっき」です"
+                        Case 12
+                            Label2.Text = "陰暦では 「師走」です"
+                            Label3.Text = "読み方は「しわす」です"
+
+                    End Select
+
+                End If
+
+            Catch ex As Exception
+                MsgBox(" 1-12 だけ整数でお願いします")
+            End Try
+
+
+        End If
+
+    End Sub
 End Class
