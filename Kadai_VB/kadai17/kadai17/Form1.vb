bf91f6d6a0c9cc3fd3bc6bc0dfaa0f3a8661657c
kagaya25 <jm.kagaya@gmail.com>
2018-09-21 10:00:23 +0900

    Done
    Done

--- Kadai_VB/kadai17/kadai17/Form1.vb
+++ Kadai_VB/kadai17/kadai17/Form1.vb
@@ -1,6 +1,13 @@
 ﻿Public Class Form1
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
 
+        Dim num1 As Integer = TextBox1.Text
+        Dim num2 As Integer = TextBox2.Text
+        Dim total As Integer = num1 * num2
+        Label3.Text = "掛け算の結果はーー＞" & total
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
     End Sub
 End Class
