649f3a99197491f7e4b4f3541909f86484ad4976
kagaya25 <jm.kagaya@gmail.com>
2019-01-10 11:57:07 +0900

    Done
    Done

--- kadai30/kadai30/Form1.Designer.vb
+++ kadai30/kadai30/Form1.Designer.vb
@@ -22,51 +22,56 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
         Me.DateTimePicker1 = New System.Windows.Forms.DateTimePicker()
         Me.Button1 = New System.Windows.Forms.Button()
         Me.Button2 = New System.Windows.Forms.Button()
         Me.Label1 = New System.Windows.Forms.Label()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
         Me.SuspendLayout()
         '
         'DateTimePicker1
         '
-        Me.DateTimePicker1.Location = New System.Drawing.Point(130, 96)
+        Me.DateTimePicker1.Location = New System.Drawing.Point(62, 72)
         Me.DateTimePicker1.Name = "DateTimePicker1"
         Me.DateTimePicker1.Size = New System.Drawing.Size(200, 19)
         Me.DateTimePicker1.TabIndex = 0
+        Me.DateTimePicker1.Value = New Date(2018, 12, 18, 11, 35, 0, 0)
         '
         'Button1
         '
-        Me.Button1.Location = New System.Drawing.Point(356, 96)
+        Me.Button1.Location = New System.Drawing.Point(294, 72)
         Me.Button1.Name = "Button1"
         Me.Button1.Size = New System.Drawing.Size(75, 23)
         Me.Button1.TabIndex = 1
-        Me.Button1.Text = "Button1"
+        Me.Button1.Text = "Set"
         Me.Button1.UseVisualStyleBackColor = True
         '
         'Button2
         '
-        Me.Button2.Location = New System.Drawing.Point(356, 215)
+        Me.Button2.Location = New System.Drawing.Point(294, 150)
         Me.Button2.Name = "Button2"
         Me.Button2.Size = New System.Drawing.Size(75, 23)
         Me.Button2.TabIndex = 2
-        Me.Button2.Text = "Button2"
+        Me.Button2.Text = "Stop"
         Me.Button2.UseVisualStyleBackColor = True
         '
         'Label1
         '
         Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(161, 186)
+        Me.Label1.Location = New System.Drawing.Point(77, 150)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(38, 12)
+        Me.Label1.Size = New System.Drawing.Size(0, 12)
         Me.Label1.TabIndex = 3
-        Me.Label1.Text = "Label1"
+        '
+        'Timer1
+        '
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(464, 450)
+        Me.ClientSize = New System.Drawing.Size(464, 239)
         Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.Button2)
         Me.Controls.Add(Me.Button1)
@@ -77,9 +82,9 @@ Partial Class Form1
         Me.PerformLayout()
 
     End Sub
-
-    Friend WithEvents DateTimePicker1 As DateTimePicker
     Friend WithEvents Button1 As Button
     Friend WithEvents Button2 As Button
     Friend WithEvents Label1 As Label
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents DateTimePicker1 As DateTimePicker
 End Class
--- kadai30/kadai30/Form1.vb
+++ kadai30/kadai30/Form1.vb
@@ -1,3 +1,34 @@
 ﻿Public Class Form1
 
+    Private SetmyTime As New DateTimePicker
+    Private Sub DateTimePicker1_ValueChanged(sender As Object, e As EventArgs) Handles DateTimePicker1.ValueChanged
+        DateTimePicker1.Format = DateTimePickerFormat.Time
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        SetmyTime.Format = DateTimePickerFormat.Time
+        DateTimePicker1.Format = DateTimePickerFormat.Time
+        SetmyTime = DateTimePicker1
+        Timer1.Start()
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Timer1.Stop()
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+
+        Me.DateTimePicker1.Value = Date.Now
+
+        If (SetmyTime.Value <= DateTimePicker1.Value) Then
+
+            Me.Label1.Text = "時間です"
+
+        End If
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
 End Class
