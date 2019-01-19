79bc2def6daec6b60196f0624102b1654ca2542b
jm.kagaya@gmail.com <jm.kagaya@gmail.com>
2019-01-19 14:49:52 +0900

    Fix later
    fix later

--- kadai43/kadai43/Form1.Designer.vb
+++ kadai43/kadai43/Form1.Designer.vb
@@ -28,7 +28,7 @@ Partial Class Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.ClientSize = New System.Drawing.Size(345, 322)
         Me.Name = "Form1"
         Me.Text = "加賀屋　ジャンメデル"
         Me.ResumeLayout(False)
