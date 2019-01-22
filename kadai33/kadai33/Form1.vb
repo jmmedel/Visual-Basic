24dc1a95a615186ebd94a60b63895010a30a43f7
kagaya25 <jm.kagaya@gmail.com>
2019-01-23 01:41:46 +0900

    Done
    Done

--- kadai33/kadai33/Form1.vb
+++ kadai33/kadai33/Form1.vb
@@ -1,3 +1,20 @@
 ﻿Public Class Form1
 
+    Dim a As ArrayList = New ArrayList
+    Dim AllItem As Integer = a.Count()
+
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        a.Add(TextBox1.Text)
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        a.Clear()
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim count As String = AllItem.ToString
+        Label2.Text += count
+    End Sub
 End Class
