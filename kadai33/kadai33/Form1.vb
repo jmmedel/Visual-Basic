a452f92b190671a305534c9a9b51a12ff7fcdad5
kagaya25 <jm.kagaya@gmail.com>
2019-01-23 01:47:49 +0900

    done
    done

--- kadai33/kadai33/Form1.vb
+++ kadai33/kadai33/Form1.vb
@@ -5,7 +5,8 @@
 
 
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-        a.Add(TextBox1.Text)
+        Dim number As Integer = Convert.ToInt32(TextBox1.Text)
+        a.Add(number)
     End Sub
 
     Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
@@ -17,4 +18,5 @@
         Dim count As String = AllItem.ToString
         Label2.Text += count
     End Sub
+
 End Class
