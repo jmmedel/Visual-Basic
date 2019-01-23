b32482a5ba284027c2d0c7c92da2c08d3f4604e6
kagaya25 <jm.kagaya@gmail.com>
2019-01-24 01:54:01 +0900

    問5
    問⑤　課題

--- vbtest04/vbtest04/Form1.vb
+++ vbtest04/vbtest04/Form1.vb
@@ -1,22 +1,28 @@
 ﻿Public Class Form1
+
+    Dim total As Integer = 0
+    Dim lunch As Integer = 900
+    Dim cake As Integer = 400
+    Dim cofee As Integer = 500
     Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
         CheckBox1.Checked = False
         CheckBox2.Checked = False
         CheckBox3.Checked = False
-
+        total = 0
     End Sub
 
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
 
 
         If (CheckBox1.Checked) Then
-
+            total += lunch
         ElseIf (CheckBox2.Checked) Then
-
+            total += cake
         ElseIf (CheckBox3.Checked) Then
-
+            total += cofee
         End If
 
+        Label2.Text = total
 
 
     End Sub
