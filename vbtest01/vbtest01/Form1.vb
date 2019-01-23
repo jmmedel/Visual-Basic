3a0fb16bf74fd74585db69538d01b6a07d1f21b1
kagaya25 <jm.kagaya@gmail.com>
2019-01-24 01:12:15 +0900

    Done
    Done

---
+++ vbtest01/vbtest01/Form1.vb
@@ -0,0 +1,18 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Label1.Text = "0H01007"
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Label1.Text = "出席番号表示"
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Label2.Text = "加賀屋　ジャンメデル"
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        Label2.Text = "名前表示"
+
+    End Sub
+End Class
